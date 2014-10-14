923c92e257f774c1ae55cc8a16ae0bdfa345d0a4
Matthew Wilson <matt@labzero.com>
2014-10-13 17:06:31 -0700

    adding a vanilla capistrano rig to deploy our app

---
+++ config/deploy/test.rb
@@ -0,0 +1,9 @@
+set :rails_env, 'production'
+
+server 'example.com', user: 'ubuntu', roles: %w{web app db}, primary: true
+
+set :ssh_options, {
+    keys: %w(~/.ssh/fhlb-test.pem),
+    forward_agent: false,
+    auth_methods: %w(publickey)
+}
