89fdd1cbc32a5ea4e64dd3197618fa4e68fe199b
kgbpimp <kalimanb@fhlbsf.com>
2014-10-30 15:21:52 -0700

    Created static Previous Activity module on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,10 @@ class DashboardController < ApplicationController
 
   def index
     @previous_activity = [
-        ['Overnight VRC Open', '$44,503,000', 'SEPT 3, 2014'],
-        ['Overnight VRC Open', '39,097,000', 'SEPT 2, 2014'],
-        ['Overnight VRC Open', '37,990,040', 'AUG 12, 2014'],
-        ['Overnight VRC Open', '39,282,021', 'FEB 14, 2014']
+        [t('dashboard.previous_activity.overnight_vrc'), 44503000, DateTime.new(2014,9,3)],
+        [t('dashboard.previous_activity.overnight_vrc'), 39097000, DateTime.new(2014,9,2)],
+        [t('dashboard.previous_activity.overnight_vrc'), 37990040, DateTime.new(2014,8,12)],
+        [t('dashboard.previous_activity.overnight_vrc'), 39282021, DateTime.new(2014,2,14)]
     ]
 
   end
