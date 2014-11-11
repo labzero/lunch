d59398a43cfbc5be23853ef2053648970b7f5b49
Edward Holets <ned@labzero.com>
2014-11-11 11:21:57 -0800

    Added market overview graph with representative data.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require highcharts
 //= require_tree .
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -6,5 +6,12 @@ RSpec.describe DashboardController, :type => :controller do
       get :index
       expect(response.body).to render_template("index")
     end
+    it "should assign @market_overview" do
+      get :index
+      expect(assigns[:market_overview]).to be_present
+      expect(assigns[:market_overview][0]).to be_present
+      expect(assigns[:market_overview][0][:name]).to be_present
+      expect(assigns[:market_overview][0][:data]).to be_present
+    end
   end
 end
