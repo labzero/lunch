f1089ef7f22665f9f65297fd6533fcbea0789813
Brian Johnson <brian.leroy.johnson@gmail.com>
2014-11-12 17:16:18 -0800

    Add Total Securities and Borrowing Capacity graphs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -45,18 +45,40 @@ class DashboardController < ApplicationController
     }];
 
     # this info will likely be coming from the model layer, which will query the CDB.  Just throwing fake data in here for now
+    # pledged collateral data
     mortgages = 67574000 #probably be something like "mortgages = user.pledged_collateral.mortgages"
     agency = 3000000
     aaa = 15000000
     aa = 5000000
     total_collateral = mortgages + agency + aaa + aa
 
+    # total securities data
+    pledged_securities = 40
+    safekept_securities = 124
+    total_securities = pledged_securities + safekept_securities
+
+    # effective borrowing capacity
+    used_capacity = 18800000
+    unused_capacity = 105000000
+    total_capacity = used_capacity + unused_capacity
+    threshold_capacity = 35
 
     @pledged_collateral = {
         mortgages: {absolute: mortgages, percentage: mortgages.fdiv(total_collateral)*100},
         agency: {absolute: agency, percentage: agency.fdiv(total_collateral)*100},
         aaa: {absolute: aaa, percentage: aaa.fdiv(total_collateral)*100},
         aa: {absolute: aa, percentage: aa.fdiv(total_collateral)*100}
+    }
+
+    @total_securities = {
+        pledged_securities: {absolute: pledged_securities, percentage: pledged_securities.fdiv(total_securities)*100},
+        safekept_securities: {absolute: safekept_securities, percentage: safekept_securities.fdiv(total_securities)*100}
+    }
+
+    @effective_borrowing_capacity = {
+        used_capacity: {absolute: used_capacity, percentage: used_capacity.fdiv(total_capacity)*100},
+        unused_capacity: {absolute: unused_capacity, percentage: unused_capacity.fdiv(total_capacity)*100},
+        threshold_capacity: threshold_capacity
     }
 
   end
