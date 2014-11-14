c2fede40548f3ef37214a9742f0651b0a29c9f4e
Brian Johnson <brian.leroy.johnson@gmail.com>
2014-11-13 16:14:12 -0800

    Added ClientBalanceService and fakes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
 
+  MEMBER_ID = 750 #this is the hard-coded fhlb client id number we're using for the time-being
+  THRESHOLD_CAPACITY = 35 #this will be set by each client, probably with a default value of 35, and be stored in some as-yet-unnamed db
+
   def index
     @previous_activity = [
       [t('dashboard.previous_activity.overnight_vrc'), 44503000, DateTime.new(2014,9,3)],
@@ -44,42 +47,11 @@ class DashboardController < ApplicationController
       data: RatesService.new.overnight_vrc
     }];
 
-    # this info will likely be coming from the model layer, which will query the CDB.  Just throwing fake data in here for now
-    # pledged collateral data
-    mortgages = 67574000 #probably be something like "mortgages = user.pledged_collateral.mortgages"
-    agency = 3000000
-    aaa = 15000000
-    aa = 5000000
-    total_collateral = mortgages + agency + aaa + aa
+    client_balances = ClientBalanceService.new(MEMBER_ID)
 
-    # total securities data
-    pledged_securities = 40
-    safekept_securities = 124
-    total_securities = pledged_securities + safekept_securities
-
-    # effective borrowing capacity
-    used_capacity = 18800000
-    unused_capacity = 105000000
-    total_capacity = used_capacity + unused_capacity
-    threshold_capacity = 35
-
-    @pledged_collateral = {
-        mortgages: {absolute: mortgages, percentage: mortgages.fdiv(total_collateral)*100},
-        agency: {absolute: agency, percentage: agency.fdiv(total_collateral)*100},
-        aaa: {absolute: aaa, percentage: aaa.fdiv(total_collateral)*100},
-        aa: {absolute: aa, percentage: aa.fdiv(total_collateral)*100}
-    }
-
-    @total_securities = {
-        pledged_securities: {absolute: pledged_securities, percentage: pledged_securities.fdiv(total_securities)*100},
-        safekept_securities: {absolute: safekept_securities, percentage: safekept_securities.fdiv(total_securities)*100}
-    }
-
-    @effective_borrowing_capacity = {
-        used_capacity: {absolute: used_capacity, percentage: used_capacity.fdiv(total_capacity)*100},
-        unused_capacity: {absolute: unused_capacity, percentage: unused_capacity.fdiv(total_capacity)*100},
-        threshold_capacity: threshold_capacity
-    }
+    @pledged_collateral = client_balances.pledged_collateral
+    @total_securities = client_balances.total_securities
+    @effective_borrowing_capacity = client_balances.effective_borrowing_capacity.merge!({threshold_capacity: THRESHOLD_CAPACITY}) # we'll be pulling threshold capacity from a different source than the ClientBalanceService
 
   end
 
