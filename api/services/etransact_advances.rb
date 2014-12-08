37eb3ed6a2f7f4b53b06c7bf14d7a11537d84b2b
siew Lee-Rowsell <rowsells@fhlbsf.com>
2014-12-08 12:14:48 -0800

    recommit mem161 to new branch
    redo

---
+++ api/services/etransact_advances.rb
@@ -0,0 +1,87 @@
+module MAPI
+  module Services
+    module EtransactAdvances
+      include MAPI::Services::Base
+
+      def self.registered(app)
+        @connection = ActiveRecord::Base.establish_connection('cdb').connection if app.environment == 'production'
+
+        service_root '/etransact_advances', app
+        swagger_api_root :etransact_advances do
+
+          # etransact advances status endpoint
+          api do
+            key :path, '/status'
+            operation do
+              key :method, 'GET'
+              key :summary, 'Retrieve status of etransact Advances today'
+              key :notes, 'Return status if etransact is turn on for the day and if all products reached it end time for the day'
+              key :type, :etransactAdvancesStatus
+              key :nickname, :getEtransactAdvancesStatus
+              response_message do
+                key :code, 200
+                key :message, 'OK'
+              end
+            end
+          end
+        end
+
+        # etransact advances status
+        relative_get "/status" do
+          etransact_advances_turn_on_string = <<-SQL
+            SELECT count(*) as status_on_count
+            FROM WEB_ADM.AO_SETTINGS
+            WHERE SETTING_NAME = 'StartUp'
+            AND trunc(to_date(SETTING_VALUE, 'MM/dd/yyyy')) = trunc(sysdate)
+          SQL
+
+          etransact_advances_bucket_on_string = <<-SQL
+            SELECT AO_TERM_BUCKET_ID, WHOLE_LOAN_ENABLED AS WL_VRC_status
+            FROM WEB_ADM.AO_TERM_BUCKETS
+            WHERE (END_TIME || '00' > to_char(sysdate, 'HH24MISS') OR
+            ((trunc(OVERRIDE_END_DATE) = trunc(sysdate))
+            AND (OVERRIDE_END_TIME || '00' > TO_CHAR(SYSDATE, 'HH24MISS')))
+            AND (WHOLE_LOAN_ENABLED = 'Y' OR SBC_AGENCY_ENABLED = 'Y' OR SBC_AAA_ENABLED = 'Y' OR SBC_AA_ENABLED = 'Y')
+          SQL
+
+          if @connection
+            etransact_status_on_cursor = @connection.execute(etransact_advances_turn_on_string)
+            etransact_status = false
+            while row = etransact_status_on_cursor.fetch()
+              if row[0].to_i == 1
+                etransact_status = true
+              end
+            end
+            if etransact_status == false # no need to check term bucket as etransact has not turn on for the day
+              {
+                etransact_status: etransact_status,
+                etransact_bucket_status: false,
+                wl_vrc_status: false
+              }.to_json
+            else
+              etransact_b_status_on_cursor = @connection.execute(etransact_advances_bucket_on_string)
+              etransact_bucket_status = false
+              wl_vrc_status = false
+              etran
+              while row = etransact_b_status_on_cursor.fetch()
+                etransact_bucket_status = true # there is some bucket available
+                if row[0].to_i == 1  # ao_term_bucket_id = 1 is for whole loan overnight
+                   if row[1] == 'Y'
+                     wl_vrc_status = true #wl_vrc_status = 1
+                   end
+                end
+              end
+              {
+                  etransact_status: etransact_status,
+                  etransact_bucket_status: etransact_bucket_status,
+                  wl_vrc_status: wl_vrc_status
+              }.to_json
+            end
+            else
+            File.read(File.join(MAPI.root, 'fakes', 'etransact_advances_status.json'))
+          end
+        end
+      end
+    end
+  end
+end
---
+++ api/spec/api/etransact_advances_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe MAPI::ServiceApp do
+   describe "etransact advances status" do
+     let(:etransact_advances_status) { get '/etransact_advances/status'; JSON.parse(last_response.body) }
+     it "should return 3 etransact advances status" do
+       expect(etransact_advances_status.length).to be >=1
+       expect(etransact_advances_status['etransact_advances_status'] == true || false)
+       expect(etransact_advances_status['etransact_bucket_status'] == true || false)
+       expect(etransact_advances_status['wl_vrc_status'] == true || false)
+     end
+   end
+end
