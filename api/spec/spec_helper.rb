a9b47b008adc3a109c23b0006d988dc23f7df92c
Edward Holets <ned@labzero.com>
2014-12-16 14:32:13 -0800

    Added rcov format output to SimpleCov.

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -19,6 +19,14 @@
 require 'rack/test'
 
 require 'simplecov'
+require 'simplecov-rcov'
+class SimpleCov::Formatter::MergedFormatter
+  def format(result)
+     SimpleCov::Formatter::HTMLFormatter.new.format(result)
+     SimpleCov::Formatter::RcovFormatter.new.format(result)
+  end
+end
+SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
 SimpleCov.start
 
 require File.expand_path '../../mapi.rb', __FILE__
