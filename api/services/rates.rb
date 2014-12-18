13628d3bcbe6117a455b392f5a50c72e816daf9d
kgbpimp <kalimanb@fhlbsf.com>
2014-12-18 14:57:33 -0800

    added maturity date and timestamp

--- api/services/rates.rb
+++ api/services/rates.rb
@@ -282,10 +282,12 @@ module MAPI
                     'payment_on' => 'Maturity',
                     'interest_day_count' => fhlbsfresponse[ctr_type].at_css('marketData FhlbsfMarketData dayCountBasis').content,
                     'day_to _maturity' => (Date.parse(fhlbsfdatapoints[ctr_term-1].at_css('tenor maturityDate').content) - Date.today).to_i,
-                    'rate' => fhlbsfdatapoints[ctr_term-1].at_css('value').content
+                    'rate' => fhlbsfdatapoints[ctr_term-1].at_css('value').content,
+                    'maturity_date' => DateTime.parse(fhlbsfdatapoints[ctr_term-1].at_css('tenor maturityDate').content)
                   }
                 end
               end
+              hash['timestamp'] = Time.now
               hash
             else
               halt 503, 'Service Unavailable'
