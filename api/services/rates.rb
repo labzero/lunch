daf9f7748db442d2c574f93d160075ea6214a2d1
kgbpimp <kalimanb@fhlbsf.com>
2014-12-18 15:47:57 -0800

    removed 'day_to _maturity'

--- api/services/rates.rb
+++ api/services/rates.rb
@@ -281,7 +281,6 @@ module MAPI
                   hash[type][term] = {
                     'payment_on' => 'Maturity',
                     'interest_day_count' => fhlbsfresponse[ctr_type].at_css('marketData FhlbsfMarketData dayCountBasis').content,
-                    'day_to _maturity' => (Date.parse(fhlbsfdatapoints[ctr_term-1].at_css('tenor maturityDate').content) - Date.today).to_i,
                     'rate' => fhlbsfdatapoints[ctr_term-1].at_css('value').content,
                     'maturity_date' => DateTime.parse(fhlbsfdatapoints[ctr_term-1].at_css('tenor maturityDate').content)
                   }
