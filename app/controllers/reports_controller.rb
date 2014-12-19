372eefb93e6fbcf6388e48b0b627dee2d2b9bcc3
Edward Holets <ned@labzero.com>
2014-12-19 14:55:43 -0800

    Added report dropdown.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,90 @@
 class ReportsController < ApplicationController
 
   def index
-
+    @reports = {
+      credit: {
+        advances_detail: {
+          updated: t('global.daily'),
+          available_history: t('global.all')
+        },
+        historical_advances_detail: {
+          updated: t('global.daily'),
+          available_history: t('reports.history.months18')
+        },
+        interest_rate: {
+          updated: t('global.daily'),
+          available_history: t('reports.history.months12')
+        },
+        letters_of_credit: {
+          updated: t('global.daily'),
+          available_history: t('global.all')
+        },
+        forward_commitments: {
+          updated: t('global.daily'),
+          available_history: t('global.all')
+        },
+        parallel_shift: {
+          updated: t('global.monthly'),
+          available_history: t('global.all')
+        }
+      },
+      collateral: {
+        borrowing_capacity: {
+          updated: t('global.daily'),
+          available_history: t('global.all')
+        },
+        mcu: {
+          updated: t('global.daily'),
+          available_history: t('global.all')
+        }
+      },
+      capital_stock: {
+        trial_balance: {
+          updated: t('global.daily'),
+          available_history: t('global.all')
+        },
+        activity: {
+          updated: t('global.daily'),
+          available_history: t('reports.history.months12')
+        },
+        dividend_transaction: {
+          updated: t('global.quarterly'),
+          available_history: t('reports.history.months36')
+        },
+        dividend_statement: {
+          updated: t('global.quarterly'),
+          available_history: t('reports.history.present2007')
+        }
+      },
+      settlement: {
+        account: {
+          updated: t('global.daily'),
+          available_history: t('global.all')
+        }
+      },
+      securities: {
+        transactions: {
+          updated: t('reports.twice_daily'),
+          available_history: t('global.all')
+        },
+        cash_projections: {
+          updated: t('global.daily'),
+          available_history: t('global.current_day')
+        },
+        current: {
+          updated: t('reports.continuously'),
+          available_history: t('global.current_day')
+        },
+        monthly: {
+          updated: t('global.monthly'),
+          available_history: t('reports.history.months18')
+        },
+        services_monthly: {
+          updated: t('global.monthly'),
+          available_history: t('reports.history.months18')
+        }
+      }
+    }
   end
 
 end
--- features/step_definitions/reports_steps.rb
+++ features/step_definitions/reports_steps.rb
@@ -1,5 +1,5 @@
 When(/^I click on the reports link in the header$/) do
-  page.find('.page-header .secondary-nav a', text: 'Reports').click
+  page.find('.page-header .secondary-nav a', text: I18n.t('reports.title')).click
 end
 
 Then(/^I should see "(.*?)" as the report page's main title$/) do |title|
@@ -8,4 +8,18 @@ end
 
 Then(/^I should see a table of "(.*?)" reports$/) do |title|
   page.assert_selector('.reports-table th', text: title)
+end
+
+Given(/^I don't see the reports dropdown$/) do
+  report_menu = page.find('.nav-menu', text: I18n.t('reports.title'))
+  report_menu.parent.assert_selector('.nav-dropdown', visible: :hidden)
+end
+
+When(/^I hover on the reports link in the header$/) do
+  page.find('.nav-menu', text: I18n.t('reports.title')).hover
+end
+
+Then(/^I should see the reports dropdown$/) do
+  report_menu = page.find('.nav-menu', text: I18n.t('reports.title'))
+  report_menu.parent.assert_selector('.nav-dropdown', visible: true)
 end
