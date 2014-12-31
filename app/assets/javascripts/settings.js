2aa584c27a9fda13c66a65805ccfb8dd9d2358af
Brian Johnson <brian.leroy.johnson@gmail.com>
2014-12-31 10:05:36 -0800

    Added functionality to settings save button.

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,16 +1,34 @@
 $(function () {
+  var settingsSaveButton = $('.settings-email .save-button');
   $('.settings-email input[type="checkbox"]').on('click', function(event){
-    var $this = $(this);
-    var id = $this.attr('id');
-    var checked = $this[0].checked;
-    var data = {};
-
-    $this.closest('tr').toggleClass('settings-item-checked');
-    data['cookies'] = {};
-    data['cookies'][id] = checked;
-    $.post('/settings/save', data, function(response){
-      $('.settings-save-message-timestamp').html(response['timestamp']);
-      $('.settings-save-message').show();
-    });
+    var settings = getEmailSettings();
+    $(this).closest('tr').toggleClass('settings-item-checked');
+    saveSettings(settings, true);
   });
+
+  settingsSaveButton.on('click', function(event){
+    var settings = getEmailSettings();
+    settingsSaveButton.on('saved-settings', location.reload());
+    saveSettings(settings);
+  });
+
+  function getEmailSettings() {
+    var settings = {};
+    settings['cookies'] = {};
+    $('.settings-email input').each(function(i, input){
+      var id = $(input).attr('id');
+      settings['cookies'][id] = input.checked;
+    });
+    return settings;
+  };
+
+  function saveSettings(data, autosave) {
+    $.post('/settings/save', data, function(response){
+      if (autosave) {
+        $('.settings-save-message-timestamp').html(response['timestamp']);
+        $('.settings-save-message').show();
+      };
+      response['status'] === 200 ? settingsSaveButton.trigger('saved-settings') : settingsSaveButton.trigger('error-saving-settings');
+    });
+  };
 });
