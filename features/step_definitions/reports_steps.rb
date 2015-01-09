3373deeafcda57aca5e0a9a2d3d159f3633d8045
Brian Johnson <brian.leroy.johnson@gmail.com>
2015-01-09 11:07:04 -0800

    Modified report_steps

--- features/step_definitions/reports_steps.rb
+++ features/step_definitions/reports_steps.rb
@@ -56,16 +56,21 @@ When(/^I click the Date column heading$/) do
 end
 
 Then(/^I should see the "(.*?)" column values in "(.*?)" order$/) do |column_name, sort_order|
+  column_index = page.evaluate_script("$('thead th:contains(#{column_name})').index()") + 1 # will throw error if column name not present, which is good
+  first_row_text = page.find("tbody tr:first-child td:nth-child(#{column_index})").text
+  last_row_text = page.find("tbody tr:last-child td:nth-child(#{column_index})").text
+
   case column_name
-    when "Date"
-      top_val = Date.strptime(page.find("tbody tr:first-child td:nth-child(1)").text, '%m/%d/%Y')
-      bottom_val = Date.strptime(page.find("tbody tr:last-child td:nth-child(1)").text, '%m/%d/%Y')
-    when "Certificate Sequence"
-      top_val = page.find("tbody tr:first-child td:nth-child(2)").text.to_i
-      bottom_val = page.find("tbody tr:last-child td:nth-child(2)").text.to_i
-    else
-      raise "column_name not recognized"
+  when "Date"
+    top_val = Date.strptime(first_row_text, '%m/%d/%Y')
+    bottom_val = Date.strptime(last_row_text, '%m/%d/%Y')
+  when "Certificate Sequence"
+    top_val = first_row_text.to_i
+    bottom_val = last_row_text.to_i
+  else
+    raise "column_name not recognized"
   end
+  
   if sort_order == 'ascending'
     expect(top_val < bottom_val).to eq(true)
   elsif sort_order == 'descending'
@@ -74,3 +79,4 @@ Then(/^I should see the "(.*?)" column values in "(.*?)" order$/) do |column_nam
     raise 'sort_order not recognized'
   end
 end
+
