300bf327832b9215d1d3decef5153dd7a0037017
siew Lee-Rowsell <rowsells@fhlbsf.com>
2015-01-13 10:47:33 -0800

    updated to return status for term & type as stated in the rates mapi
    using the  MAPI::Services::Rates::LOAN_TERMS &  MAPI::Services::Rates::LOAN_TYPES constant for now.

--- api/services/etransact_advances.rb
+++ api/services/etransact_advances.rb
@@ -2,7 +2,75 @@ module MAPI
   module Services
     module EtransactAdvances
       include MAPI::Services::Base
+      include MAPI::Services::Rates
       STATUS_ON_RECORD_NOTFOUND_COUNT = 0
+
+      TERM_BUCKET_MAPPING = {
+        :overnight => {
+          term_bucket_id: 1,
+          term_bucket_label: 'Open and O/N'
+        },
+        :open => {
+          term_bucket_id: 1,
+          term_bucket_label: 'Open and O/N'
+        },
+        :'1week'=> {
+          term_bucket_id: 2,
+          term_bucket_label: '1 Week'
+        },
+        :'2week'=> {
+          term_bucket_id: 3,
+          term_bucket_label: '2 Weeks'
+        },
+        :'3week'=> {
+        term_bucket_id: 4,
+        term_bucket_label: '3 Weeks'
+        },
+        :'1month'=> {
+        term_bucket_id: 5,
+        term_bucket_label: '1 Month'
+        },
+        :'2month'=> {
+            term_bucket_id: 6,
+            term_bucket_label: '2 Months'
+        },
+        :'3month'=> {
+            term_bucket_id: 7,
+            term_bucket_label: '3 Months'
+        },
+        :'6month'=> {
+          term_bucket_id: 8,
+          term_bucket_label: '4-6 Months'
+        },
+        :'1year'=> {
+            term_bucket_id: 11,
+            term_bucket_label: '1 Year'
+        },
+        :'2year'=> {
+            term_bucket_id: 12,
+            term_bucket_label: '1 Year'
+        },
+        :'3year'=> {
+            term_bucket_id: 13,
+            term_bucket_label: '1 Year'
+        }
+      }
+
+      TYPE_BUCKET_COLUMN_NO_MAPPING = {
+          :whole => {
+              colunn_no: 2
+          },
+          :agency => {
+              colunn_no: 3
+          },
+          :aaa=> {
+              colunn_no: 4
+          },
+          :aa=> {
+              colunn_no: 5
+          }
+      }
+
 
       def self.registered(app)
 
@@ -49,6 +117,13 @@ module MAPI
             WHERE WHOLE_LOAN_ENABLED = 'Y' AND AO_TERM_BUCKET_ID = 1
           SQL
 
+          etransact_advances_all_product_on_string = <<-SQL
+            SELECT AO_TERM_BUCKET_ID, TERM_BUCKET_LABEL,
+            WHOLE_LOAN_ENABLED, SBC_AGENCY_ENABLED, SBC_AAA_ENABLED, SBC_AA_ENABLED,
+            END_TIME, trunc(OVERRIDE_END_DATE) AS OVERRIDE_END_DATE, OVERRIDE_END_TIME
+            FROM WEB_ADM.AO_TERM_BUCKETS
+          SQL
+
           if settings.environment == :production
             etransact_status = false  #indicat if etransact is turn on and at least one product has not reach End Time
             wl_vrc_status = false   #indicate if WL VRC is enabled regardless of if etransact is turn on
@@ -75,13 +150,93 @@ module MAPI
                 break
               end
             end
+            results2 = []
+            hash = []
+            etransct_all_product_on_cursor = ActiveRecord::Base.connection.execute(etransact_advances_all_product_on_string)
+            while row = etransct_all_product_on_cursor.fetch()
+              hash = [row[0], row[1], row[2], row[3], row[4], row[5], row[6] , row[7], row[8]]
+              results2.push(hash)
+            end
           else
+            results2 = []
             results = JSON.parse(File.read(File.join(MAPI.root, 'fakes', 'etransact_advances_status.json'))).with_indifferent_access
             etransact_status = results[:etransact_advances_status]
             wl_vrc_status = results[:wl_vrc_status]
+            result_jason = JSON.parse(File.read(File.join(MAPI.root, 'fakes', 'etransact_advances_term_buckets_info.json')))
+            result_jason.each do |row|
+              hash = [row[0], row[1], row[2], row[3], row[4], row[5], row[6] , Date.parse(row[7]), row[8]]
+              results2.push(hash)
+            end
+           end
+          # # loop thru to get status for each of the type and term
+          loan_status = {}
+          LOAN_TERMS.each do |term|
+            lookup_term = TERM_BUCKET_MAPPING[term] || 0  #default to 0 if not found
+            if lookup_term.to_s != '0' then
+              lookup_term_id = TERM_BUCKET_MAPPING[term][:term_bucket_id] || 0
+            end
+            loan_status[term] ||= {}
+            LOAN_TYPES.each do |type|
+              trade_status = false
+              display_status = false
+              bucket_label = 'NotFound'
+              results2.each do |row|
+                if lookup_term_id == row[0] then
+                  bucket_label = row[1].to_s
+                  # logic to check if manually turn off regardless of end time
+                  # based on Types, will read different column
+                  lookup_column = 99 # a dummy number which has no matching column
+                 case type.to_s
+                  when 'whole'
+                    lookup_column = 2
+                  when 'agency'
+                    lookup_column = 3
+                  when 'aaa'
+                    lookup_column = 4
+                  when 'aa'
+                    lookup_column = 5
+                 end
+                  if row[lookup_column.to_i].to_s == 'Y' then
+                    display_status = true
+                  else
+                    display_status = false
+                    trade_status = false
+                    break
+                  end
+                  # logic to check end time
+                  # check if there is override for today
+                  end_time = row[6].to_s
+                  override_date = row[7].to_s
+                  override_end_time = row[8]
+                  if (Date.parse(override_date) == DateTime.now.to_date) then
+                    #check with override_end_time
+                    override_end_time += "00"  #add seconds values
+                    if (override_end_time >  DateTime.now.strftime("%H%M%S")) then
+                      trade_status = true
+                    else
+                      trade_status = false
+                    end
+                  else
+                   #check with end_time
+                    end_time +=  "00"  #add seconds values
+                    if (end_time >  DateTime.now.strftime("%H%M%S")) then
+                      trade_status = true
+                    else
+                      trade_status = false
+                    end
+                  end
+                end
+               end
+              loan_status[term][type] =
+                  {'trade_status' => trade_status,
+                   'display_status' => display_status,
+                  'bucket_label' => bucket_label
+                   }
+            end
           end
           { etransact_advances_status: etransact_status,
-            wl_vrc_status: wl_vrc_status
+            wl_vrc_status: wl_vrc_status,
+            all_loan_status: loan_status
           }.to_json
         end
       end
--- api/spec/api/etransact_advances_spec.rb
+++ api/spec/api/etransact_advances_spec.rb
@@ -6,10 +6,171 @@ describe MAPI::ServiceApp do
   end
   describe "etransact advances status" do
     let(:etransact_advances_status) { get '/etransact_advances/status'; JSON.parse(last_response.body) }
-    it "should return 3 etransact advances status" do
+    it "should return 2 etransact advances status" do
       expect(etransact_advances_status.length).to be >=1
       expect(etransact_advances_status['etransact_advances_status'] == true || false)
       expect(etransact_advances_status['wl_vrc_status'] == true || false)
+    end
+    it 'should return all_loan_status hash' do
+      expect(etransact_advances_status['all_loan_status'].length).to be >=1
+    end
+    it 'should return the expected status type and label for ALL LOAN_TERMS, LOAN_TYPES' do
+      result = etransact_advances_status['all_loan_status']
+      MAPI::Services::Rates::LOAN_TERMS.each do |term|
+         MAPI::Services::Rates::LOAN_TYPES.each do |type|
+             expect(result[term.to_s][type.to_s]['trade_status']== true || false)
+             expect(result[term.to_s][type.to_s]['display_status']== true || false)
+             expect(result[term.to_s][type.to_s]['bucket_label']).to be_kind_of(String)
+         end
+       end
+    end
+    describe 'in the production environment for cases when etransact is turned off' do
+      let!(:some_status_data) {[1, 'Open and O/N', 'N', 'Y', 'Y', 'Y', '2000', '01-JAN-2006 12:00 AM', '0700']}
+      let(:result_set) {double('Oracle Result Set', fetch: nil)}
+      let(:result_set2) {double('Oracle Result Set', fetch: nil)}
+      let(:result_set3) {double('Oracle Result Set', fetch: nil)}
+      before do
+        expect(MAPI::ServiceApp).to receive(:environment).at_least(1).times.and_return(:production)
+        allow(ActiveRecord::Base).to receive(:connection).and_return(double('OCI8 Connection'))
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(kind_of(String)).and_return(result_set)
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(kind_of(String)).and_return(result_set2)
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(kind_of(String)).and_return(result_set3)
+        allow(result_set).to receive(:fetch).and_return(0, nil)
+        allow(result_set2).to receive(:fetch).and_return(1, nil)
+        allow(result_set3).to receive(:fetch).and_return(some_status_data, nil)
+      end
+      it 'should still return the expected status type and label for ALL LOAN_TERMS, LOAN_TYPES' do
+        expect(etransact_advances_status.length).to be >=1
+        expect(etransact_advances_status['etransact_advances_status'] ==  true||false)
+        expect(etransact_advances_status['wl_vrc_status'] == true||false)
+        result = etransact_advances_status['all_loan_status']
+        MAPI::Services::Rates::LOAN_TERMS.each do |term|
+          MAPI::Services::Rates::LOAN_TYPES.each do |type|
+            expect(result[term.to_s][type.to_s]['trade_status']==  true||false)
+            expect(result[term.to_s][type.to_s]['display_status']== true||false)
+            expect(result[term.to_s][type.to_s]['bucket_label']).to be_kind_of(String)
+          end
+        end
+      end
+      it 'should return false status if no data found' do
+          expect(result_set).to receive(:fetch).and_return(nil)
+          expect(result_set2).to receive(:fetch).and_return(nil)
+          expect(result_set3).to receive(:fetch).and_return(nil)
+          expect(etransact_advances_status.length).to be >=1
+          expect(etransact_advances_status['etransact_advances_status']).to be false
+          expect(etransact_advances_status['wl_vrc_status']).to be false
+          result = etransact_advances_status['all_loan_status']
+          MAPI::Services::Rates::LOAN_TERMS.each do |term|
+            MAPI::Services::Rates::LOAN_TYPES.each do |type|
+              expect(result[term.to_s][type.to_s]['trade_status']).to be false
+              expect(result[term.to_s][type.to_s]['display_status']).to be false
+              expect(result[term.to_s][type.to_s]['bucket_label']).to eq("NotFound")
+           end
+         end
+      end
+    end
+    describe 'in the production enviroment for cases when etransact is turned on' do
+      let!(:some_status_data) {[1, 'Open and O/N', 'Y', 'Y', 'Y', 'Y', '0600', '01-JAN-2006 12:00 AM', '0700']}
+      let!(:some_status_data2) {[2, '1 Week', 'Y', 'Y', 'Y', 'N', '2000', '01-JAN-2006 12:00 AM', '0700']}
+      let!(:some_status_data3) {[3, '2 Week', 'Y', 'Y', 'Y', 'Y', '0600', DateTime.now.to_date, '2000']}
+      let(:result_set) {double('Oracle Result Set', fetch: nil)}
+      let(:result_set2) {double('Oracle Result Set', fetch: nil)}
+      let(:result_set3) {double('Oracle Result Set', fetch: nil)}
+      let(:result_set4) {double('Oracle Result Set', fetch: nil)}
+      before do
+        expect(MAPI::ServiceApp).to receive(:environment).at_least(1).times.and_return(:production)
+        allow(ActiveRecord::Base).to receive(:connection).and_return(double('OCI8 Connection'))
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(kind_of(String)).and_return(result_set)
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(kind_of(String)).and_return(result_set2)
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(kind_of(String)).and_return(result_set3)
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(kind_of(String)).and_return(result_set4)
+        allow(result_set).to receive(:fetch).and_return(1, nil)
+        allow(result_set2).to receive(:fetch).and_return(1, nil)
+        allow(result_set3).to receive(:fetch).and_return(1, nil)
+        allow(result_set4).to receive(:fetch).and_return(some_status_data, some_status_data2, some_status_data3, nil)
+      end
+      it 'should return the expected status type and label for ALL LOAN_TERMS, LOAN_TYPES' do
+        expect(etransact_advances_status.length).to be >=1
+      end
+      it 'should return etransact turn off as all products reach end time eventhought etransact is turned on' do
+        expect(etransact_advances_status['etransact_advances_status']).to be true
+        expect(etransact_advances_status['wl_vrc_status']).to be true
+      end
+      it 'should return the expected status type and label for ALL LOAN_TERMS, LOAN_TYPES' do
+        result = etransact_advances_status['all_loan_status']
+        MAPI::Services::Rates::LOAN_TERMS.each do |term|
+          MAPI::Services::Rates::LOAN_TYPES.each do |type|
+            expect(result[term.to_s][type.to_s]['trade_status'] == true || false)
+            expect(result[term.to_s][type.to_s]['display_status'] == true || false)
+            expect(result[term.to_s][type.to_s]['bucket_label']).to be_kind_of(String)
+          end
+        end
+      end
+      it 'should return trade status false if passed end time but display_status is true and there is no override for today' do
+        result = etransact_advances_status['all_loan_status']
+        MAPI::Services::Rates::LOAN_TERMS.each do |term|
+          MAPI::Services::Rates::LOAN_TYPES.each do |type|
+            if term.to_s == 'overnight' then
+              expect(result[term.to_s][type.to_s]['trade_status']).to be false
+              expect(result[term.to_s][type.to_s]['display_status']). to be true
+              expect(result[term.to_s][type.to_s]['bucket_label']).to eq('Open and O/N')
+            end
+          end
+        end
+      end
+      it 'should return trade status and display status to be false if type and term is disabled' do
+        result = etransact_advances_status['all_loan_status']
+        MAPI::Services::Rates::LOAN_TERMS.each do |term|
+          MAPI::Services::Rates::LOAN_TYPES.each do |type|
+            if term.to_s == '1week' then
+              if type.to_s == 'aa' then
+                expect(result[term.to_s][type.to_s]['trade_status']).to be false
+                expect(result[term.to_s][type.to_s]['display_status']). to be false
+              else
+                expect(result[term.to_s][type.to_s]['trade_status']).to be true
+                expect(result[term.to_s][type.to_s]['display_status']). to be true
+              end
+              expect(result[term.to_s][type.to_s]['bucket_label']).to eq('1 Week')
+            end
+          end
+        end
+      end
+      it 'should return trade status and display status to true if override_end_time for the override_end_date is not over regardless of end_time' do
+        result = etransact_advances_status['all_loan_status']
+        MAPI::Services::Rates::LOAN_TERMS.each do |term|
+          MAPI::Services::Rates::LOAN_TYPES.each do |type|
+            if term.to_s == "2week" then
+              expect(result[term.to_s][type.to_s]['trade_status']).to be true
+              expect(result[term.to_s][type.to_s]['display_status']).to be true
+              expect(result[term.to_s][type.to_s]['bucket_label']).to eq('2 Week')
+            end
+          end
+        end
+      end
+      it 'should return etransact status = false if etransact is turn on but no product is available' do
+        expect(result_set).to receive(:fetch).and_return(1, nil).at_least(1).times
+        expect(result_set2).to receive(:fetch).and_return(0, nil).at_least(1).times
+        expect(result_set3).to receive(:fetch).and_return(1, nil).at_least(1).times
+        expect(result_set4).to receive(:fetch).and_return([3, '2 Week', 'Y', 'Y', 'Y', 'Y', '0600', DateTime.now.to_date, '0700'], nil).at_least(1).times
+        expect(etransact_advances_status['etransact_advances_status']).to be false
+        expect(etransact_advances_status['wl_vrc_status']).to be true
+      end
+      it 'should return trade status to false if override_end_time for the override_end_date that is set for today has passed the current time' do
+        expect(result_set).to receive(:fetch).and_return(1, nil).at_least(1).times
+        expect(result_set2).to receive(:fetch).and_return(0, nil).at_least(1).times
+        expect(result_set3).to receive(:fetch).and_return(1, nil).at_least(1).times
+        expect(result_set4).to receive(:fetch).and_return([3, '2 Week', 'Y', 'Y', 'Y', 'Y', '0600', DateTime.now.to_date, '0700'], nil).at_least(1).times
+        result = etransact_advances_status['all_loan_status']
+        MAPI::Services::Rates::LOAN_TERMS.each do |term|
+          MAPI::Services::Rates::LOAN_TYPES.each do |type|
+            if term.to_s == "2week" then
+              expect(result[term.to_s][type.to_s]['trade_status']).to be false
+              expect(result[term.to_s][type.to_s]['display_status']).to be true
+              expect(result[term.to_s][type.to_s]['bucket_label']).to eq('2 Week')
+            end
+          end
+        end
+      end
     end
   end
 end
