14bff77fc200216ddb0dd2f812cff781592ba644
siew Lee-Rowsell <rowsells@fhlbsf.com>
2015-01-16 14:04:39 -0800

    code cleaning up

--- api/services/etransact_advances.rb
+++ api/services/etransact_advances.rb
@@ -6,69 +6,25 @@ module MAPI
       STATUS_ON_RECORD_NOTFOUND_COUNT = 0
 
       TERM_BUCKET_MAPPING = {
-        :overnight => {
-          term_bucket_id: 1,
-          term_bucket_label: 'Open and O/N'
-        },
-        :open => {
-          term_bucket_id: 1,
-          term_bucket_label: 'Open and O/N'
-        },
-        :'1week'=> {
-          term_bucket_id: 2,
-          term_bucket_label: '1 Week'
-        },
-        :'2week'=> {
-          term_bucket_id: 3,
-          term_bucket_label: '2 Weeks'
-        },
-        :'3week'=> {
-        term_bucket_id: 4,
-        term_bucket_label: '3 Weeks'
-        },
-        :'1month'=> {
-        term_bucket_id: 5,
-        term_bucket_label: '1 Month'
-        },
-        :'2month'=> {
-            term_bucket_id: 6,
-            term_bucket_label: '2 Months'
-        },
-        :'3month'=> {
-            term_bucket_id: 7,
-            term_bucket_label: '3 Months'
-        },
-        :'6month'=> {
-          term_bucket_id: 8,
-          term_bucket_label: '4-6 Months'
-        },
-        :'1year'=> {
-            term_bucket_id: 11,
-            term_bucket_label: '1 Year'
-        },
-        :'2year'=> {
-            term_bucket_id: 12,
-            term_bucket_label: '2 Years'
-        },
-        :'3year'=> {
-            term_bucket_id: 13,
-            term_bucket_label: '3 Years'
-        }
+        :overnight => 1,
+        :open => 1,
+        :'1week'=> 2,
+        :'2week'=> 3,
+        :'3week'=> 4,
+        :'1month'=> 5,
+        :'2month'=> 6,
+        :'3month'=> 7,
+        :'6month'=> 8,
+        :'1year'=> 11,
+        :'2year'=> 12,
+        :'3year'=> 13,
       }
 
       TYPE_BUCKET_COLUMN_NO_MAPPING = {
-        :whole => {
-          column_no: 2
-        },
-        :agency => {
-          column_no: 3
-        },
-        :aaa => {
-          column_no: 4
-        },
-        :aa => {
-          column_no: 5
-        }
+        :whole => 'WHOLE_LOAN_ENABLED',
+        :agency => 'SBC_AGENCY_ENABLED',
+        :aaa => 'SBC_AAA_ENABLED',
+        :aa => 'SBC_AA_ENABLED'
       }
 
       def self.registered(app)
@@ -150,89 +106,67 @@ module MAPI
               end
             end
             term_bucket_data_array = []
-            row_array = []
             etransct_all_product_on_cursor = ActiveRecord::Base.connection.execute(etransact_advances_all_product_on_string)
 
             while row = etransct_all_product_on_cursor.fetch_hash()
-               row_array = [row['AO_TERM_BUCKET_ID'], row['TERM_BUCKET_LABEL'], row['WHOLE_LOAN_ENABLED'], row['SBC_AGENCY_ENABLED'],
-                            row['SBC_AAA_ENABLED'], row['SBC_AA_ENABLED'], row['END_TIME'] , row['OVERRIDE_END_DATE'], row['OVERRIDE_END_TIME']]
-
-               term_bucket_data_array.push(row_array)
+              term_bucket_data_array.push(row)
             end
           else
             term_bucket_data_array = []
             results = JSON.parse(File.read(File.join(MAPI.root, 'fakes', 'etransact_advances_status.json'))).with_indifferent_access
             etransact_status = results[:etransact_advances_status]
             wl_vrc_status = results[:wl_vrc_status]
-            result_jason = JSON.parse(File.read(File.join(MAPI.root, 'fakes', 'etransact_advances_term_buckets_info.json')))
-           result_jason.each do |row|
-              row_array = [row['AO_TERM_BUCKET_ID'], row['TERM_BUCKET_LABEL'], row['WHOLE_LOAN_ENABLED'], row['SBC_AGENCY_ENABLED'],
-                           row['SBC_AAA_ENABLED'], row['SBC_AA_ENABLED'], row['END_TIME'] , row['OVERRIDE_END_DATE'], row['OVERRIDE_END_TIME']]
-              term_bucket_data_array.push(row_array)
-           end
-           end
+            term_bucket_data_array = JSON.parse(File.read(File.join(MAPI.root, 'fakes', 'etransact_advances_term_buckets_info.json')))
+          end
           # # loop thru to get status for each of the type and term
           loan_status = {}
-          today_date = Time.now.in_time_zone(MAPI::Shared::Constants::ETRANSACT_TIME_ZONE)
-          now_string = today_date.strftime("%H%M%S")
+          now = Time.now.in_time_zone(MAPI::Shared::Constants::ETRANSACT_TIME_ZONE)
+          now_string = now.strftime("%H%M%S")
+          today_date = now.to_date
 
           MAPI::Shared::Constants::LOAN_TERMS.each do |term|
-            lookup_term = TERM_BUCKET_MAPPING[term] || 0  #default to 0 if not found
-            if lookup_term.to_s != '0' then
-              lookup_term_id = TERM_BUCKET_MAPPING[term][:term_bucket_id] || 0
-            end
+            lookup_term_id = TERM_BUCKET_MAPPING[term]
             loan_status[term] ||= {}
             MAPI::Shared::Constants::LOAN_TYPES.each do |type|
               trade_status = false
               display_status = false
               bucket_label = 'NotFound'
               term_bucket_data_array.each do |row|
-                if lookup_term_id == row[0] then
-                  bucket_label = row[1].to_s
+                if lookup_term_id == row['AO_TERM_BUCKET_ID'].to_i
+                  bucket_label = row['TERM_BUCKET_LABEL']
                   # logic to check if manually turn off regardless of end time
                   # based on Types, will read different column
-                  lookup_column = nil # a dummy number which has no matching column
-                  lookup_column= TYPE_BUCKET_COLUMN_NO_MAPPING[type][:column_no]
-
-                  if row[lookup_column.to_i].to_s == 'Y' then
+                  lookup_column = TYPE_BUCKET_COLUMN_NO_MAPPING[type]
+                  if row[lookup_column] == 'Y'
                     display_status = true
                   else
-                    display_status = false
-                    trade_status = false
                     break
                   end
                   # logic to check end time
                   # check if there is override for today
-                  end_time = row[6].to_s
-                  override_date = row[7].to_s
-                  override_end_time = row[8]
-                  if (Date.parse(override_date) == today_date.to_date) then
-                    #check with override_end_time
-                    override_end_time += "00"  #add seconds values
-                    if (override_end_time >  now_string) then
+                  end_time = row['END_TIME'] + '00'
+                  override_date = row['OVERRIDE_END_DATE']
+                  override_end_time = row['OVERRIDE_END_TIME'] + '00'
+                  if (override_date.to_date == today_date)
+                    # check with override_end_time
+                    if (override_end_time > now_string)
                       trade_status = true
-                    else
-                      trade_status = false
                     end
-                  else
-                   #check with end_time
-                    end_time +=  "00"  #add seconds values
-                    if (end_time >  now_string) then
-                      trade_status = true
-                    else
-                      trade_status = false
-                    end
+                  elsif (end_time > now_string)
+                    trade_status = true
                   end
                 end
-               end
-              loan_status[term][type] =
-                  {'trade_status' => trade_status,
-                   'display_status' => display_status,
-                  'bucket_label' => bucket_label
-                   }
+              end
+              loan_status[term][type] = {
+                'trade_status' => trade_status,
+                'display_status' => display_status,
+                'bucket_label' => bucket_label.to_s
+              }
             end
+
           end
-          { etransact_advances_status: etransact_status,
+          {
+            etransact_advances_status: etransact_status,
             wl_vrc_status: wl_vrc_status,
             all_loan_status: loan_status
           }.to_json
