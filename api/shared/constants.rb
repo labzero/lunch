ad8ba153c87898a1a06186f4a45169ff8a9d32a6
siew Lee-Rowsell <rowsells@fhlbsf.com>
2015-01-15 17:57:25 -0800

    as per Ned comment add single constant file and rows  accessed by  column name

---
+++ api/shared/constants.rb
@@ -0,0 +1,10 @@
+module MAPI
+  module Shared
+    module Constants
+      LOAN_TYPES = [:whole, :agency, :aaa, :aa]
+      LOAN_TERMS = [:overnight, :open, :'1week', :'2week', :'3week', :'1month', :'2month', :'3month', :'6month', :'1year', :'2year', :'3year']
+
+      ETRANSACT_TIME_ZONE = 'Pacific Time (US & Canada)'
+    end
+  end
+end
