5bd276a782e2c9ffa720ef5fc5c3b189b797d374
Brian Johnson <brian.leroy.johnson@gmail.com>
2015-01-22 13:29:14 -0800

    Dropdown now a partial. Includes input field.

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -7,15 +7,21 @@ $(function(){
     if ( $dropdown.has(event.target).length == 0 && !$dropdown.is(event.target) ) {
       $('.dropdown').removeClass('open');
     } else {
-      var $parentDropdown = $(event.target).parents('.dropdown') || [];
-      var selectedValue = $(event.target).data('dropdown-value');
-      if ($parentDropdown.length > 0) {
+      var $target = $(event.target);
+      var $parentDropdown;
+      if ($target.hasClass('dropdown')) {
+        $parentDropdown = $target;
+      } else {
+        $parentDropdown = $target.parents('.dropdown') || [];
+      };
+      var selectedValue = $target.data('dropdown-value');
+      if ($parentDropdown.length > 0 || $target.hasClass('dropdown')) {
         if ($parentDropdown.hasClass('open')) {
           $dropdown.removeClass('open');
           // if there is a value for the item you selected and it differs from the current selection, trigger event and DOM changes
           if ( selectedValue && selectedValue !== $parentDropdown.data('dropdown-value') ) {
             $parentDropdown.data('dropdown-value', selectedValue);
-            $($parentDropdown.find('.dropdown-selector')).text($(event.target).text());
+            $($parentDropdown.find('input')).val($target.text());
             var eventName = $($parentDropdown).data('dropdown-name') + '-dropdown-event';
             var dropdownEvent = {type: eventName, value: selectedValue};
             $parentDropdown.trigger(dropdownEvent);
