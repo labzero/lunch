91a39dee7034273ffc7e2cc7d6c97a0a81ac3845
Edward Holets <ned@labzero.com>
2015-02-17 13:24:31 -0800

    Added a phone number formatter and wired it into place.

--- app/helpers/custom_formatting_helper.rb
+++ app/helpers/custom_formatting_helper.rb
@@ -15,4 +15,22 @@ module CustomFormattingHelper
     date.strftime('%B %-d, %Y')
   end
 
+  def fhlb_formatted_phone_number(number, ext=nil)
+    number.gsub!(/[^0-9]/, '')
+    ext.gsub!(/[^0-9]/, '') if ext
+    raise ArgumentError.new('number too short') if number.length < 10
+    raise ArgumentError.new('number too long') if number.length > 11
+    if number.length == 11
+      if number[0] == '1'
+        number = number[1..-1]
+      else
+        raise ArgumentError.new('11 digit numbers need to start with a 1')
+      end
+    end
+    formated_number = "(#{number[0..2]}) #{number[3..5]}-#{number[6..-1]}"
+    if ext.present?
+      formated_number += ", ext. #{ext}"
+    end
+    formated_number
+  end
 end
--- spec/helpers/custom_formatting_helper_spec.rb
+++ spec/helpers/custom_formatting_helper_spec.rb
@@ -23,4 +23,34 @@ describe CustomFormattingHelper do
       expect(helper.fhlb_date_long_alpha(date)).to eq('January 2, 2015')
     end
   end
+  
+  describe '`fhlb_formatted_phone_number` method' do
+    it 'converts a 10 digit phone number into the FHLB format' do
+      expect(helper.fhlb_formatted_phone_number('1234567890')).to eq('(123) 456-7890')
+    end
+    it 'converts an 11 digit phone number into the FHLB format' do
+      expect(helper.fhlb_formatted_phone_number('11234567890')).to eq('(123) 456-7890')
+    end
+    it 'approrpiately styles extentions if provided' do
+      expect(helper.fhlb_formatted_phone_number('1234567890', '6789')).to eq('(123) 456-7890, ext. 6789')
+    end
+    it 'raises an exception if fewer than 10 digits are supplied' do
+      expect {helper.fhlb_formatted_phone_number('123456789')}.to raise_error
+    end
+    it 'raises an exception if more than 11 digits are supplied' do
+      expect {helper.fhlb_formatted_phone_number('123456789012')}.to raise_error
+    end
+    it 'raises an exception if 11 digits are supplied and the first is not a 1' do
+      expect {helper.fhlb_formatted_phone_number('21234567890')}.to raise_error
+    end
+    it 'removes non-digit characters from the supplied string' do
+      expect(helper.fhlb_formatted_phone_number('123-456 7890', '67a89')).to eq('(123) 456-7890, ext. 6789')
+    end
+    it 'does not include the ext section if a blank string was passed' do
+      expect(helper.fhlb_formatted_phone_number('11234567890', '')).to eq('(123) 456-7890')
+    end
+    it 'does not include the ext section if a string without digits was passed' do
+      expect(helper.fhlb_formatted_phone_number('11234567890', 'abc')).to eq('(123) 456-7890')
+    end
+  end
 end
