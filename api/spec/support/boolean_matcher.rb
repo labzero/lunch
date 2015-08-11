33ad47306cc56cdfae0bd240b2fef0950ee200f5
Guy Argo <guy.argo@gmail.com>
2015-08-10 17:20:52 -0700

    Update boolean_matcher.rb

--- api/spec/support/boolean_matcher.rb
+++ api/spec/support/boolean_matcher.rb
@@ -1,5 +1,5 @@
 RSpec::Matchers.define :be_boolean do
   match do |actual|
-    actual.should satisfy { |x| x == true || x == false }
+    actual.should satisfy { |x| x.is_a? TrueClass || x.is_a? FalseClass }
   end
 end
