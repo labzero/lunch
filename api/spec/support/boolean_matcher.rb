a7ce4e2d36134b6e7aa964128d7a0c6d67cb36e6
Guy Argo <guy@labzero.com>
2015-08-10 17:43:05 -0700

    fixes for PR feedback

--- api/spec/support/boolean_matcher.rb
+++ api/spec/support/boolean_matcher.rb
@@ -1,5 +1,5 @@
 RSpec::Matchers.define :be_boolean do
   match do |actual|
-    actual.should satisfy { |x| x.is_a? TrueClass || x.is_a? FalseClass }
+    expect( actual ).to satisfy { |x| x.is_a?( TrueClass ) || x.is_a?( FalseClass ) }
   end
 end
