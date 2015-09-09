ed976676f5a98ab890bef86c79fd0c06adb678c9
Adolfo Foronda <emailadolfo@gmail.com>
2015-09-08 21:25:33 -0700

    adding sbc to routes file to render

--- config/routes.rb
+++ config/routes.rb
@@ -94,7 +94,7 @@ Rails.application.routes.draw do
       get 'mortgage-partnership-finance' => 'error#standard_error', as: :mpf
       get 'other-cash-needs' => 'error#standard_error', as: :ocn
       get 'putable' => 'products#putable', as: :putable
-      get 'securities-backed-credit' => 'error#standard_error'
+      get 'securities-backed-credit' => 'error#standard_error', as: :sbc
       get 'variable-rate-credit' => 'error#standard_error', as: :vrc
     end
   end
