f5748ed9b07705391604288dfa6f96fca29a0328
Edward Holets <ned@labzero.com>
2015-09-28 13:01:56 -0700

    Consolidate service desk phone numbers.

--- app/helpers/contact_information_helper.rb
+++ app/helpers/contact_information_helper.rb
@@ -2,6 +2,7 @@ module ContactInformationHelper
 
   WEB_SUPPORT_EMAIL = 'websupport@fhlbsf.com'
   WEB_SUPPORT_PHONE_NUMBER = '4156162610'
+  SERVICE_DESK_PHONE_NUMBER = '8004443452'
 
   def web_support_email
     "mailto:#{WEB_SUPPORT_EMAIL}"
@@ -9,5 +10,9 @@ module ContactInformationHelper
 
   def web_support_phone_number
     fhlb_formatted_phone_number(WEB_SUPPORT_PHONE_NUMBER)
+  end
+
+  def service_desk_phone_number
+    fhlb_formatted_phone_number(SERVICE_DESK_PHONE_NUMBER)
   end
 end
--- spec/helpers/contact_information_helper_spec.rb
+++ spec/helpers/contact_information_helper_spec.rb
@@ -35,4 +35,22 @@ describe ContactInformationHelper, type: :helper do
       expect(call_method).to be(formatted_phone_number)
     end
   end
+
+  describe '`service_desk_phone_number` method' do
+    let(:call_method) { helper.service_desk_phone_number }
+    it 'responds to `service_desk_phone_number`' do
+      expect(helper).to respond_to(:service_desk_phone_number)
+    end
+    it 'calls `fhlb_formatted_phone_number` with the SERVICE_DESK_PHONE_NUMBER' do
+      phone_number = double('A Number')
+      stub_const("#{described_class}::SERVICE_DESK_PHONE_NUMBER", phone_number)
+      expect(helper).to receive(:fhlb_formatted_phone_number).with(phone_number)
+      call_method
+    end
+    it 'returns the formatted phone number' do
+      formatted_phone_number = double('A Formatted Number')
+      allow(helper).to receive(:fhlb_formatted_phone_number).and_return(formatted_phone_number)
+      expect(call_method).to be(formatted_phone_number)
+    end
+  end
 end
