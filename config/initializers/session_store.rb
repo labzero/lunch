f2d3e7b907777d9e39038ca2d7413b99a41d0e19
Edward Holets <ned@labzero.com>
2015-10-14 12:56:08 -0700

    Add trusted proxy configuration and refactor middleware.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -13,9 +13,3 @@ session_store_args = {
   namespace: RedisHelper.namespace_from_url(ENV['SESSION_REDIS_URL'])
 }
 Rails.application.config.session_store :redis_store, session_store_args
-
-# moved the session parser up in the middleware hierarchy
-Rails.application.config.middleware.delete(ActionDispatch::Cookies)
-Rails.application.config.middleware.delete(ActionDispatch::Session::RedisStore)
-Rails.application.config.middleware.insert_before(Rails::Rack::Logger, ActionDispatch::Session::RedisStore, session_store_args)
-Rails.application.config.middleware.insert_before(ActionDispatch::Session::RedisStore, ActionDispatch::Cookies)
