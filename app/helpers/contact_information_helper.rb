31c794ba7a04423f114e73225c3c129729252168
Edward Holets <ned@labzero.com>
2015-10-23 11:13:05 -0700

    Update IRR contact instructions.

--- app/helpers/contact_information_helper.rb
+++ app/helpers/contact_information_helper.rb
@@ -3,6 +3,7 @@ module ContactInformationHelper
   WEB_SUPPORT_EMAIL = 'websupport@fhlbsf.com'
   WEB_SUPPORT_PHONE_NUMBER = '4156162610'
   SERVICE_DESK_PHONE_NUMBER = '8004443452'
+  OPERATIONS_PHONE_NUMBER = '4156162559'
 
   def web_support_email
     "mailto:#{WEB_SUPPORT_EMAIL}"
@@ -14,5 +15,9 @@ module ContactInformationHelper
 
   def service_desk_phone_number
     fhlb_formatted_phone_number(SERVICE_DESK_PHONE_NUMBER)
+  end
+
+  def operations_phone_number
+    fhlb_formatted_phone_number(OPERATIONS_PHONE_NUMBER)
   end
 end
