c869def37e053a65e8520179e4cc466c2333c68e
Brian Johnson <brian.leroy.johnson@gmail.com>
2015-10-30 13:43:43 -0300

    `Open` term quick advances have maturity date of Open

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -4,7 +4,8 @@ describe DashboardHelper, type: :helper do
   let(:today){ Date.today}
   let(:maturity_date){ double('maturity_date',to_date: today) }
   let(:rate_data){ { interest_day_count: 1, maturity_date: maturity_date, payment_on: :payment_on} }
-  let(:result){ helper.make_quick_advance_tooltip_data(rate_data)}
+  let(:advance_term) { double('advance_term', to_sym: nil)}
+  let(:result){ helper.make_quick_advance_tooltip_data(rate_data, advance_term)}
 
   let(:label){ t('dashboard.quick_advance.tooltip.maturity_date') }
 
@@ -12,5 +13,9 @@ describe DashboardHelper, type: :helper do
     allow(I18n).to receive(:t).and_return(:label)
     allow(helper).to receive(:fhlb_date_standard_numeric).with(today).and_return(:today_formatted)
     expect(result).to eq( { label => :today_formatted } )
+  end
+  it "returns #{I18n.t('dashboard.quick_advance.table.axes_labels.open')} if it is passed an `open` advance_term" do
+    allow(advance_term).to receive(:to_sym).and_return(:open)
+    expect(result).to eq( { label => t('dashboard.quick_advance.table.axes_labels.open') } )
   end
 end
