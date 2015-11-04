417af9dfdb47436d03740919a73db82b99d985c5
Matthew Wilson <matt@labzero.com>
2015-11-04 13:41:55 -0800

    Merge pull request #667 from labzero/feature/fix-hanging-cuke-env
    Fix for hang when launching cuke background tasks.

--- features/support/utils.rb
+++ features/support/utils.rb
@@ -5,3 +5,9 @@ end
 def run_name
   [ENV['JOB_NAME'] || "Local Dev (#{`whoami`.strip})", ENV['BUILD_NUMBER'], parallel_test_number].compact.join('-')
 end
+
+def kill_background_process(thread, stdin, kill_signal='INT')
+  Process.kill(kill_signal, thread.pid) rescue Errno::ESRCH
+  stdin.close
+  thread.value # wait for the thread to finish
+end
