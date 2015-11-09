225f2256181386c35b0fceb634ba36ecacc80e83
Adolfo Foronda <emailadolfo@gmail.com>
2015-11-09 14:49:12 -0800

    Deleting footer step file. MEM-674
    Deleting file that was not supposed to be committed.

--- features/step_definitions/footer_steps.rb
+++
@@ -1,7 +0,0 @@
-When(/^I click on the privacy policy link in the footer$/) do |link|
-  click_link(I18n.t("global.privacy_policy"))
-end
-
-Then(/^I should see "(.*?)" as the page's title$/) do |text|
-  page.assert_selector('h1', text: 'Privacy Policy')
-end
