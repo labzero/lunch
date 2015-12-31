b3aeb27c0536b8a6fda52446da9782f80a4cae4d
Edward Holets <ned@labzero.com>
2015-12-31 14:35:44 -0800

    Improve LDAP instrumentation in New Relic.

--- config/initializers/newrelic.rb
+++ config/initializers/newrelic.rb
@@ -1,16 +1,37 @@
 if defined?(NewRelic::Agent)
   require 'new_relic/agent/datastores'
+  require 'json'
 
   if defined?(Net::LDAP)
-    class Net::LDAP
-      include ::NewRelic::Agent::MethodTracer
+    [
+      :add, :bind, :bind_as, :delete, :get_operation_result, :modify, :open,
+      :rename
+    ].each do |method|
+      NewRelic::Agent::Datastores.trace(Net::LDAP, method, 'LDAP')
+    end
 
-      [
-        :add, :bind, :bind_as, :delete, :get_operation_result, :modify, :open,
-        :rename, :search
-      ].each do |method|
-        add_method_tracer method
+    class Net::LDAP
+
+      def search_with_newrelic(*args, &block)
+        callback = Proc.new do |result, metrics, elapsed|
+          query = args.first
+          if query
+            query_string = JSON.dump({
+              base: query[:base].try(:to_s),
+              filter: query[:filter].try(:to_s),
+              attributes: query[:attributes].try(:to_s),
+              scope: query[:scope].try(:to_s)
+            })
+            NewRelic::Agent::Datastores.notice_statement(query_string, elapsed)
+          end
+        end
+        NewRelic::Agent::Datastores.wrap('LDAP', :search, host, callback) do
+          search_without_newrelic(*args, &block)
+        end
       end
+
+      alias_method_chain :search, :newrelic
+
     end
   end
 
@@ -19,11 +40,15 @@ if defined?(NewRelic::Agent)
 
       def call_with_newrelic(*args, &block)
         operation = args.first
-        message = args.second
+        payload = args.second
         callback = Proc.new do |result, metrics, elapsed|
-          NewRelic::Agent::Datastores.notice_statement(message.to_json, elapsed) if message
+          if payload
+            query_string = JSON.dump(payload)
+            query_string.gsub!(ENV['SOAP_SECRET_KEY'], '*****') # mask our password
+            NewRelic::Agent::Datastores.notice_statement(query_string, elapsed)
+          end
         end
-        NewRelic::Agent::Datastores.wrap('SOAP', :call, "#{service_name}###{operation}", callback) do
+        NewRelic::Agent::Datastores.wrap('SOAP', :call, "#{service_name}##{operation}", callback) do
           call_without_newrelic(*args, &block)
         end
       end
