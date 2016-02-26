7ef268fb2684d96eee3722850ec708df91638d51
Edward Holets <ned@labzero.com>
2016-02-26 12:57:50 -0800

    Fix type issue in quick reports.

--- spec/policies/quick_report_policy_spec.rb
+++ spec/policies/quick_report_policy_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe QuickReportPolicy, type: :policy do
-  let(:member) { Member.new(SecureRandom.hex) }
-  let(:quick_report_set) { double(QuickReportSet, member_id: SecureRandom.hex) }
+  let(:member_id) { rand(1..10) }
+  let(:member) { Member.new(member_id.to_s) }
+  let(:quick_report_set) { double(QuickReportSet, member_id: rand(11..99)) }
   let(:quick_report) { double(QuickReport, quick_report_set: quick_report_set) }
   subject { described_class.new(member, quick_report) }
 
@@ -12,7 +13,7 @@ RSpec.describe QuickReportPolicy, type: :policy do
     end
     context 'when the quick report is owned by the member' do
       before do
-        allow(quick_report_set).to receive(:member_id).and_return(member.member_id)
+        allow(quick_report_set).to receive(:member_id).and_return(member_id)
       end
       it { should permit_action(:download) }
     end
