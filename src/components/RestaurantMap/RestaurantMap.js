66f4e4c012bc6c64e034bcffcad398b917915bf7
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-03-15 17:01:05 -0700

    Converting RestaurantMap into pure function

--- src/components/RestaurantMap/RestaurantMap.js
+++ src/components/RestaurantMap/RestaurantMap.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import ContextHolder from '../../core/ContextHolder';
 import RestaurantContainer from '../../containers/RestaurantContainer';
@@ -6,91 +6,87 @@ import { GoogleMapLoader, GoogleMap, Marker, InfoWindow } from 'react-google-map
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './RestaurantMap.scss';
 
-class RestaurantMap extends Component {
-  static contextTypes = {
-    insertCss: PropTypes.func,
-    store: PropTypes.object
-  }
-
-  renderInfoWindow(ref, item) {
-    const boundHandleMarkerClose = this.props.handleMarkerClose.bind(this, item.id);
-
-    return (
-      <InfoWindow
-        key={`${ref}_info_window`}
-        onCloseclick={boundHandleMarkerClose}
-      >
-        <ContextHolder context={this.context}>
-          <Provider store={this.context.store}>
-            <RestaurantContainer
-              id={item.id}
-              name={item.name}
-              address={item.address}
-              votes={item.votes}
-              tags={item.tags}
-            />
-          </Provider>
-        </ContextHolder>
-      </InfoWindow>
-    );
-  }
-
-  render() {
-    return (
-      <section className={s.root}>
-        <GoogleMapLoader
-          containerElement={
-            <div
-              style={{
-                height: '100%'
-              }}
-            />
-          }
-          googleMapElement={
-            <GoogleMap
-              defaultZoom={16}
-              defaultCenter={this.props.latLng}
-              defaultOptions={{
-                scrollwheel: false
-              }}
-            >
-              {this.props.items.map((item, index) => {
-                const length = item.votes.length;
-                const label = {
-                  fontWeight: 'bold',
-                  text: ' '
-                };
-                if (item.votes.length > 9) {
-                  label.text = '✔';
-                } else if (item.votes.length > 0) {
-                  label.text = String(length);
-                }
-
-                const ref = `marker_${index}`;
-
-                const mapUiItem = this.props.mapUi[item.id] || {};
-
-                const boundHandleMarkerClick = this.props.handleMarkerClick.bind(this, item.id);
-
-                return (
-                  <Marker
-                    position={{ lat: item.lat, lng: item.lng }}
-                    label={label}
-                    key={index}
-                    title={item.name}
-                    onClick={boundHandleMarkerClick}
-                  >
-                    {mapUiItem.showInfoWindow ? this.renderInfoWindow(ref, item) : null}
-                  </Marker>
-                );
-              })}
-            </GoogleMap>
-          }
+const RestaurantMap = ({ latLng, items, mapUi, handleMarkerClick, handleMarkerClose }, context) => (
+  <section className={s.root}>
+    <GoogleMapLoader
+      containerElement={
+        <div
+          style={{
+            height: '100%'
+          }}
         />
-      </section>
-    );
-  }
-}
+      }
+      googleMapElement={
+        <GoogleMap
+          defaultZoom={16}
+          defaultCenter={latLng}
+          defaultOptions={{
+            scrollwheel: false
+          }}
+        >
+          {items.map((item, index) => {
+            const length = item.votes.length;
+            const label = {
+              fontWeight: 'bold',
+              text: ' '
+            };
+            if (item.votes.length > 9) {
+              label.text = '✔';
+            } else if (item.votes.length > 0) {
+              label.text = String(length);
+            }
+
+            const ref = `marker_${index}`;
+
+            const mapUiItem = mapUi[item.id] || {};
+
+            const boundHandleMarkerClick = handleMarkerClick.bind(this, item.id);
+
+            const renderInfoWindow = () => {
+              const boundHandleMarkerClose = handleMarkerClose.bind(this, item.id);
+
+              return (
+                <InfoWindow
+                  key={`${ref}_info_window`}
+                  onCloseclick={boundHandleMarkerClose}
+                >
+                  <ContextHolder context={context}>
+                    <Provider store={context.store}>
+                      <RestaurantContainer
+                        id={item.id}
+                        name={item.name}
+                        address={item.address}
+                        votes={item.votes}
+                        tags={item.tags}
+                      />
+                    </Provider>
+                  </ContextHolder>
+                </InfoWindow>
+              );
+            };
+
+            return (
+              <Marker
+                position={{ lat: item.lat, lng: item.lng }}
+                label={label}
+                key={index}
+                title={item.name}
+                onClick={boundHandleMarkerClick}
+              >
+                {mapUiItem.showInfoWindow ? renderInfoWindow() : null}
+              </Marker>
+            );
+          })}
+        </GoogleMap>
+      }
+    />
+  </section>
+);
+
+RestaurantMap.contextTypes = {
+  insertCss: PropTypes.func,
+  store: PropTypes.object
+};
 
 RestaurantMap.propTypes = {
   handleMarkerClick: PropTypes.func.isRequired,
