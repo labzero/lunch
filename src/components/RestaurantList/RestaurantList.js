b5390b2f40185e0e8cf6c74d94273bf62ec08f21
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-04-05 15:04:35 -0700

    add Scroll.Element to each RestaurantContainer

--- src/components/RestaurantList/RestaurantList.js
+++ src/components/RestaurantList/RestaurantList.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import FlipMove from 'react-flip-move';
+import Scroll from 'react-scroll';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './RestaurantList.scss';
 import RestaurantContainer from '../../containers/RestaurantContainer';
@@ -9,14 +10,16 @@ const RestaurantList = ({ items }) => (
     <FlipMove staggerDelayBy={40} staggerDurationBy={40}>
       {items.map(item => (
         <li className={s.item} key={`restaurantListItem_${item.id}`}>
-          <RestaurantContainer
-            id={item.id}
-            name={item.name}
-            address={item.address}
-            votes={item.votes}
-            tags={item.tags}
-            shouldShowAddTagArea
-          />
+          <Scroll.Element name={`restaurantListItem_${item.id}`}>
+            <RestaurantContainer
+              id={item.id}
+              name={item.name}
+              address={item.address}
+              votes={item.votes}
+              tags={item.tags}
+              shouldShowAddTagArea
+            />
+          </Scroll.Element>
         </li>
       ))}
     </FlipMove>
