d6a028f8306f58a3e07775f3375df92305147ff9
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-04-10 19:02:45 -0700

    Autofocus inputs when forms appear

--- src/components/RestaurantAddTagForm/RestaurantAddTagForm.js
+++ src/components/RestaurantAddTagForm/RestaurantAddTagForm.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Autosuggest from 'react-autosuggest';
 import { getSuggestionValue, renderSuggestion } from '../../helpers/TagAutosuggestHelper';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
@@ -9,39 +9,46 @@ const returnTrue = () => true;
 
 autosuggestTheme.input = 'form-control input-sm';
 
-export const _RestaurantAddTagForm = ({
-  addTagAutosuggestValue,
-  setAddTagAutosuggestValue,
-  addNewTagToRestaurant,
-  tags,
-  handleSuggestionSelected,
-  hideAddTagForm
-}) => (
-  <form className={s.root} onSubmit={addNewTagToRestaurant}>
-    <Autosuggest
-      suggestions={tags}
-      focusInputOnSuggestionClick={false}
-      getSuggestionValue={getSuggestionValue}
-      renderSuggestion={renderSuggestion}
-      inputProps={{
-        value: addTagAutosuggestValue,
-        onChange: setAddTagAutosuggestValue
-      }}
-      theme={autosuggestTheme}
-      onSuggestionSelected={handleSuggestionSelected}
-      shouldRenderSuggestions={returnTrue}
-    />
-    <button
-      className={`btn btn-sm btn-primary ${s.button}`}
-      type="submit"
-      disabled={addTagAutosuggestValue === ''}
-      onClick={addNewTagToRestaurant}
-    >
-      add
-    </button>
-    <button className={`btn btn-sm btn-default ${s.button}`} type="button" onClick={hideAddTagForm}>cancel</button>
-  </form>
-);
+class _RestaurantAddTagForm extends Component {
+  componentDidMount() {
+    this._input.focus();
+  }
+
+  render() {
+    return (
+      <form className={s.root} onSubmit={this.props.addNewTagToRestaurant}>
+        <Autosuggest
+          suggestions={this.props.tags}
+          focusInputOnSuggestionClick={false}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={{
+            value: this.props.addTagAutosuggestValue,
+            onChange: this.props.setAddTagAutosuggestValue,
+            ref: i => { this._input = i; }
+          }}
+          theme={autosuggestTheme}
+          onSuggestionSelected={this.props.handleSuggestionSelected}
+          shouldRenderSuggestions={returnTrue}
+        />
+        <button
+          className={`btn btn-sm btn-primary ${s.button}`}
+          type="submit"
+          disabled={this.props.addTagAutosuggestValue === ''}
+        >
+          add
+        </button>
+        <button
+          className={`btn btn-sm btn-default ${s.button}`}
+          type="button"
+          onClick={this.props.hideAddTagForm}
+        >
+          cancel
+        </button>
+      </form>
+    );
+  }
+}
 
 _RestaurantAddTagForm.propTypes = {
   addNewTagToRestaurant: PropTypes.func.isRequired,
--- src/components/RestaurantNameForm/RestaurantNameForm.js
+++ src/components/RestaurantNameForm/RestaurantNameForm.js
@@ -1,34 +1,46 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './RestaurantNameForm.scss';
 
-const RestaurantNameForm = ({
-  editNameFormValue,
-  setEditNameFormValue,
-  changeRestaurantName,
-  hideEditNameForm
-}) => (
-  <form className={s.root}>
-    <span className={s.inputContainer}>
-      <input className="form-control input-sm" value={editNameFormValue} onChange={setEditNameFormValue} />
-    </span>
-    <button
-      type="submit"
-      className={`btn btn-primary btn-sm ${s.button}`}
-      onClick={changeRestaurantName}
-      disabled={editNameFormValue === ''}
-    >
-      ok
-    </button>
-    <button
-      type="button"
-      className={`btn btn-default btn-sm ${s.button}`}
-      onClick={hideEditNameForm}
-    >
-      cancel
-    </button>
-  </form>
-);
+class RestaurantNameForm extends Component {
+  componentDidMount() {
+    // React Bootstrap steals focus, grab it back
+    const input = this._input;
+    setTimeout(() => {
+      input.focus();
+    }, 1);
+  }
+
+  render() {
+    return (
+      <form className={s.root} onSubmit={this.props.changeRestaurantName}>
+        <span className={s.inputContainer}>
+          <input
+            type="text"
+            className="form-control input-sm"
+            value={this.props.editNameFormValue}
+            onChange={this.props.setEditNameFormValue}
+            ref={i => { this._input = i; }}
+          />
+        </span>
+        <button
+          type="submit"
+          className={`btn btn-primary btn-sm ${s.button}`}
+          disabled={this.props.editNameFormValue === ''}
+        >
+          ok
+        </button>
+        <button
+          type="button"
+          className={`btn btn-default btn-sm ${s.button}`}
+          onClick={this.props.hideEditNameForm}
+        >
+          cancel
+        </button>
+      </form>
+    );
+  }
+}
 
 RestaurantNameForm.propTypes = {
   editNameFormValue: PropTypes.string.isRequired,
