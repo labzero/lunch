fe22bb882e245fa7f37c9b98c7a3607ae57bfb93
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2016-05-01 08:18:59 -0700

    Show stats in dropdown
    #44

--- src/components/RestaurantDropdown/RestaurantDropdown.js
+++ src/components/RestaurantDropdown/RestaurantDropdown.js
@@ -4,7 +4,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './RestaurantDropdown.scss';
 
 const RestaurantDropdown = ({
-  id,
+  restaurant,
   listUiItem,
   decision,
   showMapAndInfoWindow,
@@ -15,28 +15,38 @@ const RestaurantDropdown = ({
 }) => {
   let editButton;
   if (!listUiItem.isEditingName) {
-    editButton = <MenuItem onSelect={showEditNameForm} key={`restaurantDropdown_${id}_editName`}>Edit name</MenuItem>;
+    editButton = (
+      <MenuItem onSelect={showEditNameForm} key={`restaurantDropdown_${restaurant.id}_editName`}>
+        Edit name
+      </MenuItem>
+    );
   }
 
   let decideButton;
-  if (decision !== null && decision.restaurant_id === id) {
+  if (decision !== null && decision.restaurant_id === restaurant.id) {
     decideButton = (
       <MenuItem
         onSelect={removeDecision}
-        key={`restaurantDropdown_${id}_removeDecision`}
+        key={`restaurantDropdown_${restaurant.id}_removeDecision`}
       >
         Remove decision
       </MenuItem>
     );
   } else {
-    decideButton = <MenuItem onSelect={decide} key={`restaurantDropdown_${id}_decide`}>Mark as decision</MenuItem>;
+    decideButton = (
+      <MenuItem onSelect={decide} key={`restaurantDropdown_${restaurant.id}_decide`}>
+        Mark as decision
+      </MenuItem>
+    );
   }
 
   const menuItems = [
-    <MenuItem onSelect={showMapAndInfoWindow} key={`restaurantDropdown_${id}_showMap`}>Reveal on map</MenuItem>,
+    <MenuItem onSelect={showMapAndInfoWindow} key={`restaurantDropdown_${restaurant.id}_showMap`}>
+      Reveal on map
+    </MenuItem>,
     editButton,
     decideButton,
-    <MenuItem onSelect={deleteRestaurant} key={`restaurantDropdown_${id}_delete`}>Delete</MenuItem>
+    <MenuItem onSelect={deleteRestaurant} key={`restaurantDropdown_${restaurant.id}_delete`}>Delete</MenuItem>
   ];
 
   const DropdownToggle = Dropdown.Toggle;
@@ -44,7 +54,7 @@ const RestaurantDropdown = ({
 
   return (
     <Dropdown
-      id={`restaurantDropdown_${id}`}
+      id={`restaurantDropdown_${restaurant.id}`}
       title=""
       bsRole="toggle"
       pullRight
@@ -55,13 +65,21 @@ const RestaurantDropdown = ({
       </DropdownToggle>
       <DropdownMenu bsRole="menu" className={s.menu}>
         {menuItems}
+        <MenuItem divider />
+        <MenuItem header>Last 4 weeks:</MenuItem>
+        <li className={s.stat}>
+          {restaurant.all_vote_count} vote{parseInt(restaurant.all_vote_count, 10) === 1 ? '' : 's'}
+        </li>
+        <li className={s.stat}>
+          {restaurant.all_decision_count} decision{parseInt(restaurant.all_decision_count, 10) === 1 ? '' : 's'}
+        </li>
       </DropdownMenu>
     </Dropdown>
   );
 };
 
 RestaurantDropdown.propTypes = {
-  id: PropTypes.number.isRequired,
+  restaurant: PropTypes.object.isRequired,
   listUiItem: PropTypes.object.isRequired,
   decision: PropTypes.object,
   showMapAndInfoWindow: PropTypes.func.isRequired,
