520fea3d3d5e228b3921ea214544b5c2ea1a8a96
Brian Johnson <bjohnson@labzero.com>
2016-05-31 13:49:14 -0700

    Add the release securities POST page. (#1081)

--- app/assets/javascripts/securities.js
+++ app/assets/javascripts/securities.js
@@ -21,4 +21,9 @@ $(function() {
       $submitButton.attr('disabled', true);
     };
   });
+
+  // Value of data attribute used in CSS to show/hide appropriate 'delivery-instructions-field'
+  $('select[name=securities_release_delivery_instructions]').on('change', function(){
+    $('.securities-delivery-instructions-fields').attr('data-selected-delivery-instruction', $(this).val());
+  });
 });
