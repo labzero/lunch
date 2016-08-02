0c398fb039319b81af5f9f7b356bb89e685e4850
Edward Holets <holetse@edwardholets.com>
2016-08-02 06:30:45 -0700

    Change display of finanacing percentage on Account Summary to reflect that its already a percentage. (#1195)

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1743,7 +1743,7 @@ class ReportsController < ApplicationController
           {
             columns: [
               {value: t('reports.pages.account_summary.financing_availability.asset_percentage')},
-              {value: (member_profile[:financing_percentage] * 100 if member_profile[:financing_percentage]), type: :percentage}
+              {value: (member_profile[:financing_percentage] if member_profile[:financing_percentage]), type: :percentage}
             ]
           },
           {
