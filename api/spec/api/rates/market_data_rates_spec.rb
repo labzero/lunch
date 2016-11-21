db7dea2c1dfed63373b7f2237408a5f99c0334ab
kgbpimp <boris.kaliman@gmail.com>
2016-11-21 13:25:33 -0800

    Fix COF lookup via MDS. (#1460)

--- api/spec/api/rates/market_data_rates_spec.rb
+++ api/spec/api/rates/market_data_rates_spec.rb
@@ -3,7 +3,7 @@ require 'date'
 
 describe MAPI::Services::Rates::MarketDataRates do
   describe "market data rates in the development environment" do
-    let(:market_data_rates) { subject.get_market_cof_rates(:development, anything)}
+    let(:market_data_rates) { subject.get_market_cof_rates(:development, anything, anything)}
     it 'should return market data rates' do
       expect(market_data_rates.length).to be >=1
       expect(market_data_rates['COF_FIXED']).to be_kind_of(String)
@@ -16,12 +16,15 @@ describe MAPI::Services::Rates::MarketDataRates do
     end
   end
   describe "market data rates in the production environment" do
-    let(:market_data_rates) { subject.get_market_cof_rates(:production, '1week')}
+    let(:market_data_rates) { subject.get_market_cof_rates(:production, '1week', 'whole')}
     let(:call_method) { market_data_rates }
     let(:mds_connection) { double('mds_connection', operations: nil) }
     let(:fhlbsfresponse) { double('fhlbsfresponse', at_css: nil, present?: true) }
     let(:response) { double('response') }
     let(:content) {'sometype'}
+    let(:frequency) { double('frequency')}
+    let(:frequency_unit) { double('frequency_unit')}
+    let(:term) { double('term')}
     it 'should return market data rates', vcr: {cassette_name: 'market_data_cof_service'} do
       expect(market_data_rates.length).to be >=1
       expect(market_data_rates['COF_FIXED']).to eq('0.94')
@@ -50,6 +53,61 @@ describe MAPI::Services::Rates::MarketDataRates do
       allow(fhlbsfresponse).to receive_message_chain(:[], :css)
       allow(mds_connection).to receive(:call).and_return(response)
       expect{call_method}.to raise_error('Missing Data')
+    end
+    describe 'building the MDS message', vcr: {cassette_name: 'market_data_cof_service'} do
+      let(:mds_connection) { MAPI::Services::Rates.init_mds_connection(:production) }
+      it 'includes the caller ID' do
+        expect(mds_connection).to receive(:call).with(:get_market_data, include(message: include('v11:caller' => [{'v11:id' => ENV['MAPI_COF_ACCOUNT']}])) ).and_call_original
+        call_method
+      end
+      it 'includes one request for each COF type' do
+        expect(MAPI::Services::Rates::COF_TYPES.length).to be > 0
+        requests = MAPI::Services::Rates::COF_TYPES.collect do |type|
+          include(
+            'v1:marketData' => [include('v12:name' => type, 'v12:pricingGroup' => [{'v12:id' => 'Live'}])],
+            'v1:caller' => [{'v11:id' => ENV['MAPI_COF_ACCOUNT']}]
+          )
+        end
+        expect(mds_connection).to receive(:call).with(:get_market_data, include(message: include('v1:requests' => [{'v1:fhlbsfMarketDataRequest' => requests}])) ).and_call_original
+        call_method
+      end
+      it 'includes interval with frequency and frequencyUnit for each tenor' do
+        term_mapping = { term => {frequency: frequency, frequency_unit: frequency_unit }}
+        stub_const 'MAPI::Shared::Constants::TERM_MAPPING', term_mapping
+        requests = MAPI::Services::Rates::COF_TYPES.collect do
+          include('v1:marketData' => [include('v12:data' => [{'v12:FhlbsfDataPoint' => ['v12:tenor' => ['v12:interval' => [{'v13:frequency' => frequency,'v13:frequencyUnit' => frequency_unit}]]]}])])
+        end
+        expect(mds_connection).to receive(:call).with(:get_market_data, include(message: include('v1:requests' => [{'v1:fhlbsfMarketDataRequest' => requests}])) ).and_call_original
+        subject.get_market_cof_rates(:production, term, 'whole')
+      end
+      it 'includes customRollingDay = 31 for whole loan types' do
+        requests = MAPI::Services::Rates::COF_TYPES.collect do
+          include('v1:marketData' => [include('v12:customRollingDay' => 31)])
+        end
+        expect(mds_connection).to receive(:call).with(:get_market_data, include(message: include('v1:requests' => [{'v1:fhlbsfMarketDataRequest' => requests}])) ).and_call_original
+        call_method
+      end
+      it 'includes customRollingDay = 0 for sbc loan types' do
+        requests = MAPI::Services::Rates::COF_TYPES.collect do
+          include('v1:marketData' => [include('v12:customRollingDay' => 0)])
+        end
+        expect(mds_connection).to receive(:call).with(:get_market_data, include(message: include('v1:requests' => [{'v1:fhlbsfMarketDataRequest' => requests}])) ).and_call_original
+        subject.get_market_cof_rates(:production, '1week', 'agency')
+      end
+      it 'includes dayCountBasis = ACT/360 for sbc loan types' do
+        requests = MAPI::Services::Rates::COF_TYPES.collect do
+          include('v1:marketData' => [include('v12:dayCountBasis' => 'ACT/360')])
+        end
+        expect(mds_connection).to receive(:call).with(:get_market_data, include(message: include('v1:requests' => [{'v1:fhlbsfMarketDataRequest' => requests}])) ).and_call_original
+        subject.get_market_cof_rates(:production, '1week', 'agency')
+      end
+      it 'includes paymentFrequency with frequency = 6 and frequencyUnit = M for sbc loan types' do
+        requests = MAPI::Services::Rates::COF_TYPES.collect do
+          include('v1:marketData' => [include('v12:paymentFrequency' => [{ 'v13:frequency' => 6, 'v13:frequencyUnit'=> 'M'}])])
+        end
+        expect(mds_connection).to receive(:call).with(:get_market_data, include(message: include('v1:requests' => [{'v1:fhlbsfMarketDataRequest' => requests}])) ).and_call_original
+        subject.get_market_cof_rates(:production, '1week', 'agency')
+      end
     end
     it "should raise and exception, if mds service is unavailable", vcr: {cassette_name: 'market_data_cof_service_unavailable'} do
       begin
