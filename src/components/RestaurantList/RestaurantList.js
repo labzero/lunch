e3a968ea92b0e518368dfd607523bfed814e9244
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-03-08 12:31:41 -0800

    Fix Chrome vote scroll issue

--- src/components/RestaurantList/RestaurantList.js
+++ src/components/RestaurantList/RestaurantList.js
@@ -1,27 +1,40 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import FlipMove from 'react-flip-move';
 import { Element as ScrollElement } from 'react-scroll';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './RestaurantList.scss';
 import RestaurantContainer from '../../containers/RestaurantContainer';
 
-const RestaurantList = ({ ids }) => (
-  <ul className={s.root}>
-    <FlipMove staggerDelayBy={40} staggerDurationBy={40}>
-      {ids.map(id => (
-        <li key={`restaurantListItem_${id}`}>
-          <ScrollElement name={`restaurantListItem_${id}`}>
-            <RestaurantContainer
-              id={id}
-              shouldShowAddTagArea
-              shouldShowDropdown
-            />
-          </ScrollElement>
-        </li>
-      ))}
-    </FlipMove>
-  </ul>
-);
+class RestaurantList extends Component {
+  componentWillUpdate() {
+    this.scrollY = window.scrollY;
+  }
+
+  componentDidUpdate() {
+    // prevent Chrome from scrolling to new position of voted restaurant
+    window.scrollTo(0, this.scrollY);
+  }
+
+  render() {
+    const { ids } = this.props;
+
+    return (
+      <FlipMove typeName="ul" className={s.root} staggerDelayBy={40} staggerDurationBy={40}>
+        {ids.map(id => (
+          <li key={`restaurantListItem_${id}`}>
+            <ScrollElement name={`restaurantListItem_${id}`}>
+              <RestaurantContainer
+                id={id}
+                shouldShowAddTagArea
+                shouldShowDropdown
+              />
+            </ScrollElement>
+          </li>
+        ))}
+      </FlipMove>
+    );
+  }
+}
 
 RestaurantList.propTypes = {
   ids: PropTypes.array.isRequired
