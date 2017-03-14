e88463b34ae862a383886a76557a8f436bc6f478
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-03-14 10:25:07 -0700

    Add ACL check for admin

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -8,27 +8,56 @@
  */
 
 import React from 'react';
+import queryString from 'query-string';
 import LayoutContainer from '../../containers/LayoutContainer';
 
 const title = 'Admin';
-const isAdmin = false;
+
+const hasRole = (user, team, role) => {
+  if (!role || user.superuser) {
+    return true;
+  }
+  const teamRole = user.roles.find(userRole => userRole.team_id === userRole.id);
+  if (!teamRole) {
+    return false;
+  }
+  switch (role) {
+    case 'admin':
+      return teamRole.type === 'admin' || teamRole.type === 'owner';
+    case 'owner':
+      return teamRole.type === 'owner';
+    default:
+      return false;
+  }
+};
 
 export default {
 
   path: '/admin',
 
-  async action() {
-    if (!isAdmin) {
-      return { redirect: '/login' };
+  async action(context) {
+    console.log(context);
+
+    const state = context.store.getState();
+    const user = state.user;
+    const team = state.team;
+
+    if (user.id && hasRole(user, team, 'admin')) {
+      const Admin = await require.ensure([], require => require('./Admin').default, 'admin');
+
+      return {
+        title,
+        chunk: 'admin',
+        component: <LayoutContainer><Admin title={title} /></LayoutContainer>,
+      };
     }
 
-    const Admin = await require.ensure([], require => require('./Admin').default, 'admin');
+    let stringifiedQuery = queryString.stringify(context.query);
+    if (stringifiedQuery) {
+      stringifiedQuery = `%3F${stringifiedQuery}`;
+    }
 
-    return {
-      title,
-      chunk: 'admin',
-      component: <LayoutContainer><Admin title={title} /></LayoutContainer>,
-    };
+    return { redirect: `/login?next=${context.path}${stringifiedQuery}` };
   },
 
 };
