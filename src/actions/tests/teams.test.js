0f2a252ba014f5091624557da61734d7f1fb2952
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-03-22 17:43:18 -0700

    Add tests for teams actions

---
+++ src/actions/tests/teams.test.js
@@ -0,0 +1,64 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions, no-underscore-dangle, import/no-duplicates */
+
+import { expect } from 'chai';
+import configureStore from 'redux-mock-store';
+import fetchMock from 'fetch-mock';
+import thunk from 'redux-thunk';
+import * as teams from '../teams';
+import { __RewireAPI__ as teamsRewireAPI } from '../teams';
+import actionCreatorStub from '../../../test/actionCreatorStub';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('actions/teams', () => {
+  let store;
+  let flashErrorStub;
+
+  beforeEach(() => {
+    store = mockStore({});
+    flashErrorStub = actionCreatorStub();
+    teamsRewireAPI.__Rewire__('flashError', flashErrorStub);
+  });
+
+  describe('createTeam', () => {
+    let payload;
+    beforeEach(() => {
+      payload = { foo: 'bar' };
+    });
+
+    describe('before fetch', () => {
+      beforeEach(() => {
+        fetchMock.mock('*', {});
+      });
+
+      it('dispatches postTeam', () => {
+        const postTeamStub = actionCreatorStub();
+        teamsRewireAPI.__Rewire__('postTeam', postTeamStub);
+
+        store.dispatch(teams.createTeam(payload));
+
+        expect(postTeamStub.calledWith(payload)).to.be.true;
+      });
+
+      it('fetches restaurant', () => {
+        store.dispatch(teams.createTeam(payload));
+
+        expect(fetchMock.lastCall()[0]).to.eq('/api/teams');
+        expect(fetchMock.lastCall()[1].body).to.eq(JSON.stringify(payload));
+      });
+    });
+
+    describe('failure', () => {
+      beforeEach(() => {
+        fetchMock.mock('*', 400);
+        return store.dispatch(teams.createTeam(payload));
+      });
+
+      it('dispatches flashError', () => {
+        expect(flashErrorStub.called).to.be.true;
+      });
+    });
+  });
+});
