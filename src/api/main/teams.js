617920393040070d99745f6742ce9dd3d60ec680
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2017-03-30 20:03:31 -0700

    Fix error message when creating team that already exists

--- src/api/main/teams.js
+++ src/api/main/teams.js
@@ -5,6 +5,9 @@ import { TEAM_LIMIT, TEAM_SLUG_REGEX } from '../../constants';
 import errorCatcher from '../helpers/errorCatcher';
 import loggedIn from '../helpers/loggedIn';
 
+const error409 = (res) =>
+  res.status(409).json({ error: true, data: { message: 'Could not create new team. It might already exist.' } });
+
 export default () => {
   const router = new Router();
 
@@ -20,7 +23,7 @@ export default () => {
         }
 
         if (reservedTeamSlugs.indexOf(slug) > -1) {
-          return res.status(409).json({ error: true, data: { message: 'Could not create new team. It might already exist.' } });
+          return error409(res);
         }
 
         if (!slug.match(TEAM_SLUG_REGEX)) {
@@ -42,8 +45,11 @@ export default () => {
 
           const json = obj.toJSON();
           return res.status(201).send({ error: false, data: json });
-        } catch (e) {
-          return errorCatcher(res, e);
+        } catch (err) {
+          if (err.name === 'SequelizeUniqueConstraintError') {
+            return error409(res);
+          }
+          return errorCatcher(res, err);
         }
       }
     );
