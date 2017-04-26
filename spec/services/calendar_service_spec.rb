005e58328137b9b7d8768439d41dce6fff19ac2a
kgbpimp <boris.kaliman@gmail.com>
2017-04-25 19:54:31 -0700

    Delete Calendar Cache (#1669)

--- spec/services/calendar_service_spec.rb
+++ spec/services/calendar_service_spec.rb
@@ -10,57 +10,37 @@ describe CalendarService do
     let(:holiday) { instance_double(String) }
     let(:response) { {holidays: [holiday]} }
     let(:call_method) { subject.holidays(start_date, end_date) }
-    before { allow(holiday).to receive(:to_date).and_return(holiday) }
+    before do
+      allow(holiday).to receive(:to_date).and_return(holiday)
+      allow(subject).to receive(:get_hash).and_return(response)
+    end
 
-    it 'fetches the `calendar_holidays` from the Rails cache with the proper key and expiry param' do
-      expect(Rails.cache).to receive(:fetch).with(CacheConfiguration.key(:calendar_holidays), expires_in: CacheConfiguration.expiry(:calendar_holidays)).and_return([holiday])
+    it 'converts the `start_date` param to `iso8601`' do
+      expect(start_date).to receive(:iso8601)
       call_method
     end
-    describe 'when `calendar_holidays` already exists in the Rails cache' do
-      before { allow(Rails.cache).to receive(:fetch).and_return(response[:holidays]) }
-
-      it 'does not call `get_hash`' do
-        expect(subject).not_to receive(:get_hash)
-        call_method
-      end
-      it 'returns the cached value' do
-        expect(call_method).to eq(response[:holidays])
-      end
+    it 'converts the `end_date` param to `iso8601`' do
+      expect(end_date).to receive(:iso8601)
+      call_method
     end
-
-    describe 'when `calendar_holidays` does not yet exist in the Rails cache' do
-      before do
-        allow(Rails.cache).to receive(:fetch).and_yield
-        allow(subject).to receive(:get_hash).and_return(response)
-      end
-
-      it 'converts the `start_date` param to `iso8601`' do
-        expect(start_date).to receive(:iso8601)
-        call_method
-      end
-      it 'converts the `end_date` param to `iso8601`' do
-        expect(end_date).to receive(:iso8601)
-        call_method
-      end
-      it 'calls `get_hash` with `:holidays` as the name arg' do
-        expect(subject).to receive(:get_hash).with(:holidays, any_args)
-        call_method
-      end
-      it 'calls `get_hash` with the proper MAPI endpoint' do
-        expect(subject).to receive(:get_hash).with(anything, "calendar/holidays/#{start_date.iso8601}/#{end_date.iso8601}")
-        call_method
-      end
-      it 'returns the `holidays` array from the results of `get_hash`' do
-        expect(call_method).to eq(response[:holidays])
-      end
-      it 'converts all of the holidays into dates' do
-        expect(holiday).to receive(:to_date)
-        call_method
-      end
-      it 'raises an error when `get_hash` returns nil' do
-        allow(subject).to receive(:get_hash).and_return(nil)
-        expect{call_method}.to raise_error(StandardError, 'There has been an error and CalendarService#holidays has encountered nil. Check error logs.')
-      end
+    it 'calls `get_hash` with `:holidays` as the name arg' do
+      expect(subject).to receive(:get_hash).with(:holidays, any_args)
+      call_method
+    end
+    it 'calls `get_hash` with the proper MAPI endpoint' do
+      expect(subject).to receive(:get_hash).with(anything, "calendar/holidays/#{start_date.iso8601}/#{end_date.iso8601}")
+      call_method
+    end
+    it 'returns the `holidays` array from the results of `get_hash`' do
+      expect(call_method).to eq(response[:holidays])
+    end
+    it 'converts all of the holidays into dates' do
+      expect(holiday).to receive(:to_date)
+      call_method
+    end
+    it 'raises an error when `get_hash` returns nil' do
+      allow(subject).to receive(:get_hash).and_return(nil)
+      expect{call_method}.to raise_error(StandardError, 'There has been an error and CalendarService#holidays has encountered nil. Check error logs.')
     end
   end
 
