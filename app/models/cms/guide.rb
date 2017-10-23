0736a5a95499edaf335889d7b56a60dca54e4bcc
Brian Johnson <bjohnson@labzero.com>
2017-10-23 17:40:34 -0400

    Pull guide last updated date from revision lists (#1877)

--- app/models/cms/guide.rb
+++ app/models/cms/guide.rb
@@ -1,5 +1,5 @@
 class Cms::Guide
-  attr_reader :cms, :guide_type
+  attr_reader :cms, :guide_type, :revisions
 
   def initialize(member_id, request, guide_type, cms=nil)
     @guide_type = guide_type
@@ -8,14 +8,16 @@ class Cms::Guide
   end
 
   def revisions
-    revisions = []
-    slices = cms.get_slices_by_type(guide_type, 'revision')
-    slices.each { |slice| revisions <<  Revision.new(slice) }
-    revisions.sort_by{ |revision| revision.last_updated }.reverse
+    @revisions ||= (
+      revisions = []
+      slices = cms.get_slices_by_type(guide_type, 'revision')
+      slices.each { |slice| revisions <<  Revision.new(slice) }
+      revisions.sort_by{ |revision| revision.last_updated }.reverse
+    )
   end
 
   def last_revised_date
-    cms.get_date(guide_type, 'last_revised_date')
+    revisions.sort_by{ |revision| revision.last_updated }.reverse.first.try(:last_updated)
   end
 
   class Revision
