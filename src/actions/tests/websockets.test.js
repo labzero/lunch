abd2b41fc5b192982e00d031617447453165dfa0
nyscherm <nyscherm@gmail.com>
2018-03-02 20:01:24 -0800

    Add more test coverage (#120)
    * Add more test coverage
    
    closes #99
    
    * remove unnecessary describe block

---
+++ src/actions/tests/websockets.test.js
@@ -0,0 +1,49 @@
+/* eslint-env mocha */
+
+import { expect } from 'chai';
+import { useFakeTimers } from 'sinon';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import proxyquire from 'proxyquire';
+import * as websockets from '../websockets';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('actions/websockets', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({});
+  });
+
+  describe('messageReceived', () => {
+
+    it('dispatches data if action is undefined', () => {
+      const payload = '{ "type": "bar" }';
+      store.dispatch(websockets.messageReceived(payload));
+      const actions = store.getActions();
+      expect(actions[0].type).to.eq('bar');
+    });
+
+    it('dispatches action with data if action type is given', () => {
+      const payload = '{"type": "RESTAURANT_POSTED"}';
+      const proxysockets = proxyquire('../websockets', {
+        './restaurants': {
+          sortRestaurants: () => ({
+           type: 'SORT_RESTAURANTS'
+          })
+        }
+      });
+      const clock = useFakeTimers();
+      store.dispatch(proxysockets.messageReceived(payload));
+      clock.tick(1000);
+
+      const actions = store.getActions();
+      expect(actions[0].type).to.eq('RESTAURANT_POSTED');
+      expect(actions[1].type).to.eq('NOTIFY');
+      expect(actions[2].type).to.eq('SORT_RESTAURANTS');
+      clock.restore();
+    });
+  });
+});
---
+++ src/helpers/tests/canChangeUser.test.js
@@ -0,0 +1,85 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+
+import { expect } from 'chai';
+import canChangeUser from '../canChangeUser';
+
+describe('helpers/canChangeUser', () => {
+	let user;
+	let userToChange;
+	let team;
+	let users;
+
+	beforeEach(() => {
+		user = {
+			name: "dev", 
+			id: 1, 
+			superuser: false, 
+			roles: [{type: "owner", team_id: 1, user_id: 1}],
+			email: "dev@labzero.com"
+		};
+		userToChange = {
+			name: "test", 
+			id: 2,  
+			type: "member", 
+			email: "test@labzero.com"
+		};
+		team = {
+			id: 1,
+		};
+		users = [
+			{
+				name: "dev", 
+				id: 1, 
+				type: "owner", 
+				email: "dev@labzero.com"
+			},
+			{
+				name: "test", 
+				id: 2,  
+				type: "member", 
+				email: "test@labzero.com"
+			}
+		];
+	});
+
+	it('returns false when user is undefined', () => {
+		user = undefined;
+		expect(canChangeUser(user, userToChange, team, users)).to.be.false;
+	});
+
+	it('returns true when user is superuser', () => {
+		user.superuser = true;
+		expect(canChangeUser(user, userToChange, team, users)).to.be.true;
+	});
+
+	it('returns false when team is undefined', () => {
+		team = undefined;
+		expect(canChangeUser(user, userToChange, team, users)).to.be.false;
+	});
+
+	describe('when user role is owner and changing their own role', () => {
+		beforeEach(() => {
+			userToChange = {
+				name: "dev", 
+				id: 1,  
+				type: "owner", 
+				email: "dev@labzero.com"
+			};
+		});
+
+		it('returns false when there are no other owners', () => {
+			expect(canChangeUser(user, userToChange, team, users)).to.be.false;
+		});
+
+		it('returns true when there are other owners', () => {
+			users[1].type = "owner";
+			expect(canChangeUser(user, userToChange, team, users)).to.be.true;
+		});
+
+	});
+
+	it('returns true when user role is owner and none of above conditions are met', () => {
+		expect(canChangeUser(user, userToChange, team, users)).to.be.true;
+	});
+});
