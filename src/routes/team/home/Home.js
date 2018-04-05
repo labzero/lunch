71e7429f510949da6ef174b02fa7b70dc7be7513
nyscherm <nyscherm@gmail.com>
2018-04-05 09:33:59 -0700

    Only send ws ping if socket is open (#165)
    * Only send ws ping if socket is open
    
    Fixes #161
    
    * clear intervals on ws close

--- src/routes/team/home/Home.js
+++ src/routes/team/home/Home.js
@@ -55,19 +55,23 @@ export class _Home extends Component {
       this.socket.onopen = this.fetchAllData;
 
       // avoid nginx proxy_read_timeout by sending a ping every 30 seconds
-      setInterval(() => {
-        this.socket.send('');
+      this.pingInterval = setInterval(() => {
+        if (this.socket.readyState === this.socket.OPEN) {
+          this.socket.send('');
+        }
       }, 1000 * 30);
     } else {
       // websocket open will handle initial fetch
       this.fetchAllData();
     }
 
-    setInterval(this.fetchAllData, 1000 * 60 * 60);
+    this.fetchAllInterval = setInterval(this.fetchAllData, 1000 * 60 * 60);
   }
 
   componentWillUnmount() {
     this.socket.close();
+    clearInterval(this.pingInterval);
+    clearInterval(this.fetchAllInterval);
   }
 
   fetchAllData = () => {
