c39c4730a0d15ae4368bf546f69aaaf697eae5dc
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2023-05-23 11:23:23 -0700

    Convert GoogleMapsLoaderContext to TypeScript

--- src/components/GoogleMapsLoaderContext/GoogleMapsLoaderContext.ts
+++ src/components/GoogleMapsLoaderContext/GoogleMapsLoaderContext.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const GoogleMapsLoaderContext = createContext();
+const GoogleMapsLoaderContext = createContext({});
 
 export default GoogleMapsLoaderContext;
