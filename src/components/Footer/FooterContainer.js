b0b6c1db97ebe3f48b16985fc4673643d86d00f7
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2023-05-23 10:12:01 -0700

    Convert Footer to TypeScript

--- src/components/Footer/FooterContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import Footer from "./Footer";
-
-const mapStateToProps = (state) => ({
-  host: state.host,
-});
-
-export default connect(mapStateToProps)(Footer);
