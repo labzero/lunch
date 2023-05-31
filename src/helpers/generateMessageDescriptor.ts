7ec55553195cd2817e882512531e2fe47797c3ff
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2023-05-31 11:35:22 -0700

    Convert generateMessageDescriptor to TypeScript

--- src/helpers/generateMessageDescriptor.ts
+++ src/helpers/generateMessageDescriptor.ts
@@ -1,12 +1,12 @@
 import * as messages from "../core/messages";
 import get from "./get";
 
-export const globalMessageDescriptor = (id) => ({
+export const globalMessageDescriptor = (id: string) => ({
   id,
   defaultMessage: get(messages.en, id),
 });
 
-export default (component) => (id) => {
+export default (component: string) => (id: string) => {
   const namespacedId = `${component}.${id}`;
   return globalMessageDescriptor(namespacedId);
 };
